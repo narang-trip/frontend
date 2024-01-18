20f3c094088cbc3208dcbf9a0e92ee831c7fefd1
yhc-key <yhcho0712@gmail.com>
2024-01-18 13:13:38 +0900

    feat: add react-spinners
    react-spinners npm 으로 인스톨, UX 증진을 위해 쉽게 로딩스피너를 추가할 수 있도록 함.

--- frontend/src/components/Widgets.jsx
+++ frontend/src/components/Widgets.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Widgets() {
   return (
     <div className="text-center">
       위젯 두는 곳
+      <hr/>
+      <ClipLoader color="d3d3d3" />
     </div>
   );
 }
-=======
-import axios from "axios"
-
-export default function Widgets() {
-    
-    return <div className="text-center">위젯 두는 곳 </div>
-}
->>>>>>> b42494c09ed3947fb6137f2e240e8a0c581002ad
