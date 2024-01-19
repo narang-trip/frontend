3d33f9d5ec0e883b77b7639e599820779d6a63c5
yhc-key <yhcho0712@gmail.com>
2024-01-19 14:02:46 +0900

    docs: add calendar feature and practice css of animation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Button from "../ui/Button";
 
 export default function HomePage() {
   return (
-    <div >
+    <div>
       <div className="text-3xl text-red-500 text-center">홈페이지</div>
+      <img src="/assets/travelBanner.jpg" className="object-cover w-[500px] h-[100px]"/>
+      <div className="w-auto">
+
+      </div>
     </div>
   );
 }
