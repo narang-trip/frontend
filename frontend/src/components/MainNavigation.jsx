a1604144f029028960658d5abf9f1d33b0eb95bc
se2develop <seahee0130@gmail.com>
2024-01-24 16:18:39 +0900

    Feat: navBar working
    Change-Id: Ia87349197d95858d88e478480aaf08de541009e5

--- frontend/src/components/MainNavigation.jsx
+++ frontend/src/components/MainNavigation.jsx
@@ -1,14 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { HomeIcon } from "@heroicons/react/24/outline";
 
 function MainNavigation() {
   return (
     <div>
-      <nav className=" flex items-center" >
-        <ul className="text-center">
-          <li className="mb-[400px]">
+      <nav className="flex items-center">
+        <ul className="mx-8">
+          <li className="relative group">
             <NavLink to="/" end>
-              Home
+              <span className="flex items-center text-black opacity-75 group-hover:opacity-100 py-4 pl-6 transition duration-300 ease-in-out">
+                <HomeIcon className="w-12 h-12 group-hover:bg-white rounded-l-lg p-2" />
+              </span>
             </NavLink>
+              <div className="hidden group-hover:block bg-white text-black absolute top-0 right-full p-2 rounded-md">
+                Home
+              </div>
           </li>
           <li>
             <NavLink to="/write">Write</NavLink>
