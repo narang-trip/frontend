7869dce066f517a963647149bf8412fe544a59cf
조예진 <cyj2825@naver.com>
2024-01-25 10:22:30 +0900

    Feat: 채팅 추가
    Change-Id: I37882adcfc353585c4b66df83d638273507e1d83

---
+++ backend/StompRabbitMQ/src/main/java/com/example/stomprabbitmq/StompRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.stomprabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompRabbitMqApplication.class, args);
+    }
+
+}
