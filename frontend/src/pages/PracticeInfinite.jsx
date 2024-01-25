7eb9504b36bd41e86e2f4a3b363e9a77f2d689d2
yhc-key <yhcho0712@gmail.com>
2024-01-25 10:44:47 +0900

    feat: infiniteScroll ing..
    Change-Id: Ib6df8200b06925fc439dd427806a607778490d3c

---
+++ frontend/src/pages/PracticeInfinite.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import TripContainer from "../components/TripContainer";
+
+const PracticePage = () => {
+  <Fragment>
+    <TripContainer />
+  </Fragment>;
+};
+
+export default PracticePage;
