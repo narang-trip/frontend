71a462fc4a621e71eb45420ba955189653551b5c
조예진 <cyj2825@naver.com>
2024-01-25 17:33:15 +0900

    Feat: fix Build error, remove eureka dependency
    Change-Id: Ifce4a14e15958e7ed715fa29295174370e7d5bb9

--- backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
@@ -2,10 +2,9 @@ package com.ssafy.messageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MessageServiceApplication {
 
 	public static void main(String[] args) {
