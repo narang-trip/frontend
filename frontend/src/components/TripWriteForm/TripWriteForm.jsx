f43804450a2b7948f08d2102bb55b6f818f623a0
se2develop <seahee0130@gmail.com>
2024-01-26 17:50:39 +0900

    Feat: WriteForm 구현중
    Change-Id: I2c5425816ad887f70222a145fb8fe50b3d5adfcf

--- frontend/src/components/TripWriteForm/TripWriteForm.jsx
+++ frontend/src/components/TripWriteForm/TripWriteForm.jsx
@@ -37,19 +37,6 @@ export default function TripWriteForm() {
     });
   };
 
-  // 모집 포지션
-  const positionList = [
-    "역할1",
-    "역할2",
-    "역할3",
-    "역할4",
-    "역할5",
-    "역할6",
-    "역할7",
-    "역할8",
-    "역할9",
-    "역할10",
-  ];
 
   const [dateRange, setDateRange] = useState([null, null]);
   // 기간 변할 때 추적하는 함수
@@ -74,43 +61,46 @@ export default function TripWriteForm() {
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <div className="border bg-stone-400 border-stone-700">
-          <div className="flex flex-col flex-wrap ">
-            <p className="mb-8 text-4xl text-center ">동행 글 작성하기</p>
-            <TitleInput value={formData.title} onChange={handleChange} />
-            <ConceptSelect value={formData.concept} onChange={handleChange} />
-            <FileUploadBox />
-            <DateRangePicker
-              dateRange={dateRange}
-              onChange={handleDateChange}
-            />
-            <div>
-              <label>여행 장소</label>
-            </div>
-            <PositionCheck value={formData.position} onChange={handleChange} />
-            <div>
-              <label>여행 계획표</label>
-            </div>
-            <div>
-              <label>여행 설명</label>
-              <input type="text"></input>
-            </div>
-            <div>
-              <button
-                className="px-6 py-2 rounded-md text-stone-800 bg-stone-500 hover:text-stone-950"
-                onClick={saveWrite}
-              >
-                저장
-              </button>
-              <button
-                className="px-6 py-2 rounded-md bg-stone-800 text-stone-50"
-                onClick={back}
-              >
-                취소
-              </button>
+          <div className="container px-8 py-8 mx-auto border bg-stone-400 border-stone-700">
+            <div className="flex flex-col flex-wrap ">
+              <p className="mb-8 text-4xl text-center ">동행 글 작성하기</p>
+              <TitleInput value={formData.title} onChange={handleChange} />
+              <ConceptSelect value={formData.concept} onChange={handleChange} />
+              <FileUploadBox />
+              <DateRangePicker
+                dateRange={dateRange}
+                onChange={handleDateChange}
+              />
+              <div>
+                <label>여행 장소</label>
+              </div>
+              <PositionCheck
+                value={formData.position}
+                onChange={handleChange}
+              />
+              <div>
+                <label>여행 계획표</label>
+              </div>
+              <div>
+                <label>여행 설명</label>
+                <textarea/>
+              </div>
+              <div>
+                <button
+                  className="px-6 py-2 rounded-md text-stone-800 bg-stone-500 hover:text-stone-950"
+                  onClick={saveWrite}
+                >
+                  저장
+                </button>
+                <button
+                  className="px-6 py-2 rounded-md bg-stone-800 text-stone-50"
+                  onClick={back}
+                >
+                  취소
+                </button>
+              </div>
             </div>
           </div>
-        </div>
       </form>
     </Fragment>
   );
