58e4cf9a0b6a25956aebabed6fb4c8c4557025f0
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-01-26 17:49:55 +0900

    Feat: 로그인 모달 이벤트버블링 막기
    Change-Id: Ieea2389fdbb34d532b929f0fe4f8061d8b781386

--- frontend/src/components/modals/LoginModal.jsx
+++ frontend/src/components/modals/LoginModal.jsx
@@ -1,23 +1,40 @@
+import { useRef } from "react";
+
 const LoginModal = (props) => {
+  const modalBG = useRef(null);
+
   return (
-    <div className="justify-center">
-      <button className="" onClick={props.onClose}>
-        x
-      </button>
-      <h3 className="">로 그 인</h3>
-      <div className="flex flex-col">
-        <button>
-          <img
-            className="w-36 h-16 object-cover rounded-xl"
-            src="assets/kakao_login.png"
-          />
-        </button>
-        <button>
-          <img
-            className="w-36 h-16 object-cover rounded-xl"
-            src="assets/naver_login.png"
-          />
-        </button>
+    <div
+      className="bg-gray-500 bg-opacity-70 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center z-0"
+      onClick={props.onClose}
+      ref={modalBG}
+    >
+      <div
+        className="bg-white px-10 py-4 rounded-3xl w-96 h-82 z-10"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className="">
+          <button className="" onClick={props.onClose}>
+            x
+          </button>
+          <h3 className="">로 그 인</h3>
+          <div className="flex flex-col">
+            <button>
+              <img
+                className="w-36 h-16 object-cover rounded-xl"
+                src="assets/kakao_login.png"
+              />
+            </button>
+            <button>
+              <img
+                className="w-36 h-16 object-cover rounded-xl"
+                src="assets/naver_login.png"
+              />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
