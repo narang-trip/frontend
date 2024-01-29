da27bdb8c32486e22437b4f909cfa5f8cfb3ef0b
yhc-key <yhcho0712@gmail.com>
2024-01-29 13:32:19 +0900

    feat: carousel animation ing...
    Change-Id: I15bccf230ac4505778fdf71db93c234f802a422e

--- frontend/src/ui/Carousel.jsx
+++ frontend/src/ui/Carousel.jsx
@@ -4,14 +4,13 @@ import { RxDotFilled } from "react-icons/rx";
 const Carousel = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(currentIndex);
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length-1;
+    const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
@@ -21,36 +20,7 @@ const Carousel = ({ slides }) => {
   };
 
   return (
-    <div className="max-w-[1000px] h-[600px] min-h-[300px] w-full m-auto py-16 px-4 border-solid border-black border-2 overflow-hidden relative flex flex-row ">
-      {slides.map((slide, index) => {
-        return (
-          <div key={index} className={`border-solid border-black border-2 absolute w-[95%] h-[500px] flex flex-wrap ${index !== currentIndex ? 'hidden' : ''}`}>
-            <div
-              style={{ backgroundImage: `url(${slide[0].url})` }}
-              className="w-[45%] h-[45%] rounded-2xl bg-center bg-cover m-2"
-            ></div>
-            <div
-              style={{
-                backgroundImage: `url(${slide[1].url})`,
-              }}
-              className="w-[45%] h-[45%] rounded-2xl bg-center bg-cover m-2 "
-            ></div>
-            <div
-              style={{
-                backgroundImage: `url(${slide[2].url})`,
-              }}
-              className=" w-[45%] h-[45%] rounded-2xl bg-center bg-cover m-2 "
-            ></div>
-            <div
-              style={{
-                backgroundImage: `url(${slide[3].url})`,
-              }}
-              className=" w-[45%] h-[45%] rounded-2xl bg-center bg-cover m-2 "
-            ></div>
-          </div>
-        );
-      })}
-
+    <div>
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-point">
         <FaArrowAltCircleLeft onClick={prevSlide} size={30} />
       </div>
