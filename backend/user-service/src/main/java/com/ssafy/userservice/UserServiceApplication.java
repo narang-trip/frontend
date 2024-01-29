28d07e7701b5b25efe44c7e7a1aa78485ea63c4c
송윤재 <1218song@naver.com>
2024-01-29 12:58:45 +0900

    Feat: add Auth
    Change-Id: I3d44afc5ee83a4bbfcb7a90e44b6928670790ea7

--- backend/user-service/src/main/java/com/ssafy/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/UserServiceApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //@EnableDiscoveryClient
 
@@ -11,6 +13,10 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
