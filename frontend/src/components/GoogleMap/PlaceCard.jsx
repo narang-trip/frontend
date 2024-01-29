b23b1be2379ec4d14809d7c207df81cd4cd546fc
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-01-29 11:04:13 +0900

    Feat: test
    Change-Id: I8d91509e1402bcd8d7af432925c87c345fd8299b

--- frontend/src/components/GoogleMap/PlaceCard.jsx
+++ frontend/src/components/GoogleMap/PlaceCard.jsx
@@ -13,39 +13,39 @@ const PlaceCard = () => {
   return (
     <DragDropContext>
       {searchResults.map((place, index) => (
-        // <Draggable draggableId={`${place}_${index}`} index={index} key={index}>
-        //   {(provided) => (
-        //     <div
-        //       ref={provided.innerRef}
-        //       {...provided.draggableProps}
-        //       {...provided.dragHandleProps}
-        //     >
-        //       test
-        <Card key={index} sx={{ maxWidth: 345 }}>
-          <CardMedia
-            sx={{ height: 200 }}
-            image={place.photo}
-            title={place.name}
-          />
-          <CardContent>
-            <Avatar src={place.icon} />
-            <Typography gutterBottom variant="h5" component="div">
-              {place.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {place.businessStatus}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {place.formattedAddress}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {place.rating}
-            </Typography>
-          </CardContent>
-        </Card>
-        //     </div>
-        //   )}
-        // </Draggable>
+        <Draggable draggableId={`${place}_${index}`} index={index} key={index}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+            >
+              test
+              <Card key={index} sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  sx={{ height: 200 }}
+                  image={place.photo}
+                  title={place.name}
+                />
+                <CardContent>
+                  <Avatar src={place.icon} />
+                  <Typography gutterBottom variant="h5" component="div">
+                    {place.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {place.businessStatus}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {place.formattedAddress}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {place.rating}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+          )}
+        </Draggable>
       ))}
     </DragDropContext>
   );
