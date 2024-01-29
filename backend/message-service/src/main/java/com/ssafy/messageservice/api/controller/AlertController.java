6444600a4843280dbcd7cda2cb26252f0616e307
조예진 <cyj2825@naver.com>
2024-01-30 08:59:50 +0900

    Feat: SSE 알림 기능 진행 중
    Change-Id: Ie7b0e8b6cdf2365fdf04662378175d6c46d666e2

--- backend/message-service/src/main/java/com/ssafy/messageservice/api/controller/AlertController.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/api/controller/AlertController.java
@@ -1,13 +1,25 @@
 package com.ssafy.messageservice.api.controller;
 
+import com.ssafy.messageservice.api.response.AlertAttendRequest;
+import com.ssafy.messageservice.api.response.ChatDto;
+import com.ssafy.messageservice.api.service.AlertService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/alert")
 public class AlertController {
+    private final AlertService alertService;
+
+    // 여행 참여 요청 생성
+    @PostMapping("/attend")
+    public ResponseEntity<String> postAttendAlert(@RequestBody AlertAttendRequest alertAttendRequest) {
+        alertService.notify(alertAttendRequest.getReceiverId(), alertAttendRequest);
+        // 성공적인 응답 반환
+        return new ResponseEntity<>("Alert sent successfully", HttpStatus.OK);
+    }
 }
---
+++ backend/message-service/src/main/java/com/ssafy/messageservice/db/repository/AlertRepository.java
@@ -0,0 +1,16 @@
+package com.ssafy.messageservice.db.repository;
+
+import com.ssafy.messageservice.db.entity.Chat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+//@Repository
+//@RequiredArgsConstructor
+//public interface AlertRepository extends JpaRepository<Chat, String> {
+//
+//}
