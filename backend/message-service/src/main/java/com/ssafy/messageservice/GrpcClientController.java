9bfe2af8e89a37a4bdc98027ee57f36f9fdbdbe4
조예진 <cyj2825@naver.com>
2024-01-29 20:03:53 +0900

    Feat: 채팅 API 마무리
    Change-Id: Ic35517b7ad1ee96e449d67dfbcd5c4fb0f9dda11

---
+++ backend/message-service/src/main/java/com/ssafy/messageservice/GrpcClientController.java
@@ -0,0 +1,17 @@
+package com.ssafy.messageservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GrpcClientController {
+    private final GrpcClientService grpcClientService;
+
+    // 되는거까지 확인
+    @GetMapping("/test")
+    public String printMessage() {
+        return grpcClientService.sendMessage("test");
+    }
+}
