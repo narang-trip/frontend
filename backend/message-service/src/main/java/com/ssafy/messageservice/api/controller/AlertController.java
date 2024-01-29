2b5ea7764a4ee27b1e37716201e8bd57589c7be9
조예진 <cyj2825@naver.com>
2024-01-29 21:33:54 +0900

    Fix: Swagger Url 수정
    Change-Id: I90281ca430ff0056cca0554836b72c17ff193677

---
+++ backend/message-service/src/main/java/com/ssafy/messageservice/api/controller/AlertController.java
@@ -0,0 +1,13 @@
+package com.ssafy.messageservice.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/alert")
+public class AlertController {
+}
