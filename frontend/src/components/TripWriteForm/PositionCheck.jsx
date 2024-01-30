c4fc604559e02f711a1daac8618f42478af0aa63
se2develop <seahee0130@gmail.com>
2024-01-30 14:34:36 +0900

    Fix: 오류 해결
    Change-Id: I009f43a7d828a64cb7016351d6f2aab4546de929

--- frontend/src/components/TripWriteForm/PositionCheck.jsx
+++ frontend/src/components/TripWriteForm/PositionCheck.jsx
@@ -16,13 +16,13 @@ export default function PositionCheck({ value, onChange }) {
   ];
 
   return (
-    <div className="w-full my-5">
-          <label className="text-xl font-medium">모집 포지션</label>
+    <div className="w-full my-2">
+          <label className="text-sm">모집 포지션</label>
           <div className="flex flex-wrap">
           {positionList.map((position, index) => (
             <div key={index} className="w-1/4 p-2">
               <input type="checkbox" value={position}></input>
-              <label className="m-2">{position}</label>
+              <label className="m-2 text-xs">{position}</label>
             </div>
           ))}
           </div>
