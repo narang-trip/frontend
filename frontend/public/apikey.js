0cb6d58cd14adf566f24bc330f5bf4877d1caacd
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-01-30 13:49:37 +0900

    Test: 배포후 로그인 테스트
    Change-Id: If78b9e5161db80e6c873e073680435fcb3600772

--- frontend/public/apikey.js
+++ frontend/public/apikey.js
@@ -1,6 +1,7 @@
 export const kakaoApiKey = "27a9fd30b37529913566120655684173";
 export const kakaoRedirectUrl =
   "https://i10a701.p.ssafy.io/api/user/login/oauth2/code/kakao";
+// 로컬에서 테스트
 // export const kakaoRedirectUrl = "http://localhost:3000/login/oauth2/code/kakao";
 
 export const naverApikey = "_cFgoA5jCXN10jJ6pAoo";
