55ff0bf9c2506c0b688b08289e2691db31be4d73
yhc-key <yhcho0712@gmail.com>
2024-01-30 13:12:04 +0900

    test: test docker
    Change-Id: I5d660634036f9f3f35c17cd555a997a62f077a18

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import TripList from "../components/TripList";
 export default function HomePage() {
   return (
     <div>
+      홈페이지입니다. 도커 테스트 중입니다.
       <img
         src="/assets/travelBanner.jpg"
         className="object-cover w-full h-40"
