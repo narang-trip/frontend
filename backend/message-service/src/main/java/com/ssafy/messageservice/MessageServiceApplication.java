cb6d6f978745cf3bc5aa85822fe98ee3f1c8df9a
조예진 <cyj2825@naver.com>
2024-01-31 16:37:22 +0900

    Fix : stomp 문제 해결중
    Change-Id: I6c93d81e88bb195733025460a35ffa6ce28f4da2

--- backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
@@ -4,7 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< Updated upstream
 
+=======
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//@EnableDiscoveryClient
+>>>>>>> Stashed changes
 @OpenAPIDefinition(servers = {@Server(url = "https://i10a701.p.ssafy.io", description = "default url")})
 @SpringBootApplication
 public class MessageServiceApplication {
