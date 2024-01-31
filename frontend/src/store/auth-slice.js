24ac409dc9abdc4658bb247c76554a077ee74c8d
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-01-31 10:13:11 +0900

    Feat: 로그인 기능 전반 구현 중
    Change-Id: I975bd4243a18bf854b6161246a3bd52a800812d7

--- frontend/src/store/auth-slice.js
+++ frontend/src/store/auth-slice.js
@@ -1,17 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isLogin : false
-}
+  isLogin: false,
+  code: "",
+};
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     Login: (state, action) => {
-      state.isLogin = true  // 로그인
+      state.isLogin = true; // 로그인
+      state.code = action.payload;
     },
-    Logout: (state, action) => {
+    Logout: (state) => {
       state.isLogin = false; // 로그아웃
+      state.code = "";
     },
   },
 });
