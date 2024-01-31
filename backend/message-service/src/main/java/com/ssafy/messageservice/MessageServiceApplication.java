92230cd63bc9ffe2ad14922d81cde25774d04071
김영섭 <kimyoungseop33@gmail.com>
2024-01-31 16:32:46 +0900

    Fix: removed Eureka depen
    Change-Id: Icade2aa2fadf2cbbda07dd67112ffcd6f481a397

--- backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/MessageServiceApplication.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @OpenAPIDefinition(servers = {@Server(url = "https://i10a701.p.ssafy.io", description = "default url")})
 @SpringBootApplication
 public class MessageServiceApplication {
