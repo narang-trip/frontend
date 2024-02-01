2a9286df169863aa80e62b3bbb17db4386e151dd
김영섭 <kimyoungseop33@gmail.com>
2024-02-02 00:13:05 +0900

    Feat: Trip Service MongoDB Conn
    Change-Id: Ie56633a5af29e61285a85b7671f90e5f6fccb339

---
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/PlanService.java
@@ -0,0 +1,4 @@
+package com.ssafy.tripservice.api.service;
+
+public interface PlanService {
+}
---
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
@@ -0,0 +1,19 @@
+package com.ssafy.tripservice.api.service;
+
+import com.ssafy.tripservice.db.entity.Trip;
+
+import java.util.List;
+
+public interface TripService {
+    /*
+        Trip 생성
+     */
+    Trip createTrip(Trip trip);
+    /*
+        현재 기준 참가 가능한 Trip
+     */
+    List<Trip> getAvailableTrips(Trip);
+
+
+
+}
