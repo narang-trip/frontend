55adeb1c70a5846ea26b70922076d81d66638528
조예진 <cyj2825@naver.com>
2024-02-02 18:19:54 +0900

    Feat: 로그 추가
    Change-Id: I79d2c31e975d0528b2734636535c7f3675d54769

--- backend/message-service/src/main/java/com/ssafy/messageservice/config/StompConfig.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/config/StompConfig.java
@@ -1,13 +1,17 @@
 package com.ssafy.messageservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
 
 @Configuration
 @EnableWebSocketMessageBroker
