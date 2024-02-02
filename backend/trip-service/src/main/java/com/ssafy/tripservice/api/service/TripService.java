1038fadead7d822d3121fb5e248c5ef99a0c5a8a
김영섭 <kimyoungseop33@gmail.com>
2024-02-02 14:16:29 +0900

    Feat: Add Trip Find, Create
    Change-Id: I498ce472176d6fdd36209bb0646722b850d121ea

--- backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
@@ -1,16 +1,22 @@
 package com.ssafy.tripservice.api.service;
 
+import com.ssafy.tripservice.api.dto.TripDto;
 import com.ssafy.tripservice.db.entity.Trip;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TripService {
     /*
         Trip 생성
      */
-    Trip createTrip(Trip trip);
+    Optional<TripDto> createTrip(TripDto tripDto);
     /*
         현재 기준 참가 가능한 Trip
      */
-    List<Trip> getAvailableTrips();
+    List<TripDto> getAvailableTrips(LocalDateTime currentTime);
+
+    Optional<TripDto> getTripById(UUID tripId);
 }
