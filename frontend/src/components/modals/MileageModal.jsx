c3c53fc05c027ec6a80b30ff4db6f398ab7be15b
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-02 09:51:58 +0900

    Feat: 마일리지 구현중
    Change-Id: I101692f205d756d77d2b0663fb4374185da0cf6a

---
+++ frontend/src/components/modals/MileageModal.jsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+
+const MileageModal = (props) => {
+  const modalBG = useRef("");
+  const [option, setOption] = useState("use");
+
+  const Change = (e) => {
+    console.log(e);
+    setOption(e);
+  };
+
+  return (
+    <div
+      className="fixed top-0 bottom-0 left-0 right-0 z-0 flex items-center justify-center bg-gray-500 bg-opacity-70"
+      onClick={props.onClose}
+      ref={modalBG}
+    >
+      <div
+        className="z-10 px-10 py-4 bg-white rounded-3xl w-96 h-82"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <button className="" onClick={props.onClose}>
+          x
+        </button>
+        <>
+          마일리지
+          <button onClick={() => Change("use")}>사용내역</button>
+          <button onClick={() => Change("get")}>획득내역</button>
+          {option === "use" ? }
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default MileageModal;
