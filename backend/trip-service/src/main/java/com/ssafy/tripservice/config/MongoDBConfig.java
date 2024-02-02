1ddcc9d3aa6d48b477a0e780591abdb09a074737
김영섭 <kimyoungseop33@gmail.com>
2024-02-02 16:44:13 +0900

    Fix: update Mongo Settings
    Change-Id: I53d52c5bdc410644c47d3feb900c37fa994eb9c7

--- backend/trip-service/src/main/java/com/ssafy/tripservice/config/MongoDBConfig.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/config/MongoDBConfig.java
@@ -1,7 +1,9 @@
 package com.ssafy.tripservice.config;
 
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.bson.UuidRepresentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoAdmin;
@@ -11,12 +13,17 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 public class MongoDBConfig {
 
     @Bean
+    MongoClientSettings mongoClientSettings() {
+        return MongoClientSettings.builder().uuidRepresentation(UuidRepresentation.STANDARD).build();
+    }
+
+    @Bean
     public MongoClient mongoClient() {
         return MongoClients.create("mongodb://mongo-container:27017");
     }
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "-mongo-narang");
+        return new MongoTemplate(mongoClient(), "mongo-narang");
     }
 }
