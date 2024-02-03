e599383ec6d35abbd0d9c04bf8996ed9cc29f780
yhc-key <yhcho0712@gmail.com>
2024-02-03 14:37:14 +0900

    test: chat test 21
    Change-Id: I23b3fe091c6ec9f014b32f9d0462ed7d3e457b94

--- frontend/src/pages/ChatRoom.jsx
+++ frontend/src/pages/ChatRoom.jsx
@@ -1,9 +1,12 @@
 import { Fragment, useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import SockJS from "sockjs-client";
-import StompJs from "@stomp/stompjs";
+import {Client} from "@stomp/stompjs";
 
 import Button from "../ui/Button";
+
+const sockjsEndpoint = 'https://i10a701.p.ssafy.io/api/message/stomp/chat';
+const stompEndpoint = 'https://i10a701.p.ssafy.io/api/message/stomp/chat';
 
 const ChatRoomPage = () => {
   const params = useParams();
@@ -23,37 +26,44 @@ const ChatRoomPage = () => {
     console.log("또 실행되면 나옴 유즈이펙트 안에 있음");
     // inputRef.current.focus();
     console.log("sockJS 실행 전");
-    const sockJS = new SockJS(
-      "/api/message/stomp/chat"
-    );
-    // const sockJS = new WebSocket("wss://i10a701.p.ssafy.io/api/message/chat")
+    const sockJS = new SockJS(sockjsEndpoint)
+    
+    const stompClient = new Client({ webSocketFactory: () => new WebSocket(stompEndpoint), debug: (str) => console.log(str) });
 
-    const stompClient = StompJs.over(sockJS);
+    stompClient.configure({
+      brokerURL: stompEndpoint,
+      onConnect: () => {
+        console.log('Connected to STOMP');
+      }
+
+      // stompClient.subcribe();
+    })
 
 
-    stompClient.connect(
-      "yoonjae",
-      "dbswoWkd",
-      function (frame) {
-        console.log("Connected to Stomp");
 
-        stompClient.subscribe(
-          `/exchange/chat.exchange/room.${chatRoomId}`,
-          function (message) {
-            const chatDto = JSON.parse(message.body);
+    // stompClient.connect(
+    //   "yoonjae",
+    //   "dbswoWkd",
+    //   function (frame) {
+    //     console.log("Connected to Stomp");
 
-            setChats((prevData) => [
-              ...prevData,
-              {
-                [chatDto.nickname]: chatDto.message,
-              },
-            ]);
-          },
-          function (error) {
-            console.error("Stomp connection error", error);
-          }
-        );
-      });
+    //     stompClient.subscribe(
+    //       `/exchange/chat.exchange/room.${chatRoomId}`,
+    //       function (message) {
+    //         const chatDto = JSON.parse(message.body);
+
+    //         setChats((prevData) => [
+    //           ...prevData,
+    //           {
+    //             [chatDto.nickname]: chatDto.message,
+    //           },
+    //         ]);
+    //       },
+    //       function (error) {
+    //         console.error("Stomp connection error", error);
+    //       }
+    //     );
+    //   });
 
     //     //입장 메세지 전송
     //     stompClient.send(
@@ -71,53 +81,53 @@ const ChatRoomPage = () => {
     // setStomp(stompClient);
   }, [chatRoomId, nickname]);
 
-const submitHandler = (event) => {
-  event.preventDefault();
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-  // stomp.send(
-  //   "/pub/chat.message.1",
-  //   {}, 
-  //   JSON.stringify({
-  //     message: inputRef.current,
-  //     memberId: 1,
-  //     nickname: "YourNickname",
-  //   })
-  // );
-  // inputRef.current.valueOf("");
-  setInputMsg("");
-};
+    // stomp.send(
+    //   "/pub/chat.message.1",
+    //   {}, 
+    //   JSON.stringify({
+    //     message: inputRef.current,
+    //     memberId: 1,
+    //     nickname: "YourNickname",
+    //   })
+    // );
+    // inputRef.current.valueOf("");
+    setInputMsg("");
+  };
 
-return (
-  <Fragment>
-    <h1>Chat Room</h1>
-    <h2>Room.No. {chatRoomId}</h2>
-    <h2>안녕하세요 {nickname}님</h2>
-    <div>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="message">Message:</label>
-        <input
-          type="text"
-          id="message"
-          ref={inputRef}
-          value={inputMsg}
-          onChange={(e) => setInputMsg(e.target.value)}
-          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <Button type="submit">전송</Button>
-      </form>
-    </div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <div className="w-64 h-80 bg-gray-300 p-4 rounded-md shadow-md">
-        <h1>채팅방 입장</h1>
-        <div id="chatArea">
-          {chats.map((msg) => (
-            <li key={msg.message}>{`${msg.nickname} : ${msg.message}`}</li>
-          ))}
+  return (
+    <Fragment>
+      <h1>Chat Room</h1>
+      <h2>Room.No. {chatRoomId}</h2>
+      <h2>안녕하세요 {nickname}님</h2>
+      <div>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="message">Message:</label>
+          <input
+            type="text"
+            id="message"
+            ref={inputRef}
+            value={inputMsg}
+            onChange={(e) => setInputMsg(e.target.value)}
+            className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <Button type="submit">전송</Button>
+        </form>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="w-64 h-80 bg-gray-300 p-4 rounded-md shadow-md">
+          <h1>채팅방 입장</h1>
+          <div id="chatArea">
+            {chats.map((msg) => (
+              <li key={msg.message}>{`${msg.nickname} : ${msg.message}`}</li>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </Fragment>
-);
-          }
+    </Fragment>
+  );
+}
 
 export default ChatRoomPage
