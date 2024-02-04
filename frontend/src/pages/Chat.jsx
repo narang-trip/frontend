a6e51cb1f804927c9359e8b5dea7ed891547fc7c
yhc-key <yhcho0712@gmail.com>
2024-02-05 00:14:45 +0900

    fix : chat test 33
    Change-Id: I808fa81d4d3c738af7365b54a76b4be8aeaf8fec

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -63,26 +63,6 @@ const ChatPage = () => {
         </label>
       </div>
 
-      <form onSubmit={submitHandler}>
-        <label htmlFor="chatRoomId">룸아이디</label>
-        <br />
-        <input
-          type="number"
-          id="chatRoomId"
-          name="chatRoomId"
-          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <hr />
-        <label htmlFor="chatRoomId">닉네임</label>
-        <br />
-        <input
-          type="text"
-          name="nickname"
-          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <hr />
-        <Button type="submit">참여하기</Button>
-      </form>
     </div>
   );
 };
