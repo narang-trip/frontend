760027cbe433905641aa33957bf9ee373b44cef9
김영섭 <kimyoungseop33@gmail.com>
2024-02-04 13:51:49 +0900

    Feat: Trip-Mongo / Trip-S3 Conn
    Change-Id: Ia5a61fb4e4e5cadd71039cb4a95174fbbfcdeeed

--- backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripServiceImpl.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripServiceImpl.java
@@ -1,12 +1,13 @@
 package com.ssafy.tripservice.api.service;
 
-import com.ssafy.tripservice.api.dto.TripDto;
+import com.ssafy.tripservice.api.request.TripRequest;
+import com.ssafy.tripservice.api.response.TripResponse;
 import com.ssafy.tripservice.db.entity.Trip;
 import com.ssafy.tripservice.db.repository.TripRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +17,33 @@ import java.util.UUID;
 @AllArgsConstructor
 public class TripServiceImpl implements TripService {
 
-
     private final TripRepository tripRepository;
 
     @Override
-    public Optional<TripDto> createTrip(TripDto tripDto) {
+    public Optional<TripResponse> createTrip(TripRequest tripRequest) {
 
-        return Optional.of(tripRepository.save(tripDto));
+        Trip trip = tripRequest.toEntity();
+
+        try {
+            tripRepository.save(trip);
+            return Optional.of(trip.toTripResponse());
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
     }
 
     @Override
-    public List<TripDto> getAvailableTrips(LocalDateTime currentTime) {
-        return tripRepository.findAll();
+    public List<TripResponse> getAvailableTrips(LocalDateTime currentTime) {
+        return tripRepository.findAll()
+                .stream().map(Trip::toTripResponse)
+                .toList();
     }
 
     @Override
-    public Optional<TripDto> getTripById(UUID tripId) {
-        return tripRepository.findById(tripId);
-    }
+    public Optional<TripResponse> getTripById(UUID tripId) {
 
+        return tripRepository.findById(tripId)
+                .map(Trip::toTripResponse);
+    }
 }
