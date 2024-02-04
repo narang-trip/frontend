b451fa27e7285e71f46b7f552a4615a644671c0e
yhc-key <yhcho0712@gmail.com>
2024-02-05 00:36:04 +0900

    test :chat test 37
    Change-Id: I8c2647165d9f3dc11317e16226d30b38498857ac

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -58,13 +58,13 @@ const ChatPage = () => {
     // SockJS와 Stomp 설정
     const socket = new SockJS(sockjsEndpoint);
     const webSocket = new WebSocket(stompEndpoint);
-    webSocket.onopen = function () {
-      console.log('서버와 웹소켓 연결 성공!');
-    };
-    webSocket.onmessage = function (event) {
-      console.log(event.data);
-      webSocket.send('클라이언트에서 서버로 답장을 보냅니다');
-    };
+    // webSocket.onopen = function () {
+    //   console.log('서버와 웹소켓 연결 성공!');
+    // };
+    // webSocket.onmessage = function (event) {
+    //   console.log(event.data);
+    //   webSocket.send('클라이언트에서 서버로 답장을 보냅니다');
+    // };
     const stompClient = new Client({
       webSocketFactory: () => webSocket, // SockJS 인스턴스를 사용하여 웹소켓 연결
       onConnect: () => {
