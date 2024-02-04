48e61c60848706d93b53af9efa46335ec9009f20
1218song <1218song@naver.com>
2024-02-05 01:07:26 +0900

    Feat: Mileage 사용 기록 기능 구현
    Change-Id: I348419a35dd1fe6b6821f6db4b3bfc552f143ca6

---
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/db/repository/UsageRecordRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.paymentservice.db.repository;
+
+import com.ssafy.paymentservice.db.entity.ChargeRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsageRecordRepository extends JpaRepository<ChargeRecord, String> {
+
+}
---
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/service/MileageService.java
@@ -0,0 +1,12 @@
+package com.ssafy.paymentservice.service;
+
+import com.ssafy.paymentservice.db.entity.UsageRecord;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MileageService {
+    public UsageRecord useMileage(String user_id, String price){
+
+        return null;
+    }
+}
