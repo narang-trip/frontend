9a9a7077678ee46ef57c59706fc93d0fd83621eb
yhc-key <yhcho0712@gmail.com>
2024-02-05 00:08:43 +0900

    fix : fix error chat
    Change-Id: Ibe24997d999edb20e236782fe5fa9f4553deae1e

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,64 +2,58 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../ui/Button";
 import SockJS from "sockjs-client";
-import { Client, Stomp} from "@stomp/stompjs";
+import { Client, Stomp } from "@stomp/stompjs";
 
 const userId = "조예진"
 const sockjsEndpoint = 'https://i10a701.p.ssafy.io/api/message/api/message/chat';
 const stompEndpoint = 'wss://i10a701.p.ssafy.io/api/message/chat';
+
 const ChatPage = () => {
-  const navigate = useNavigate();
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const chatRoomId = e.target.elements.chatRoomId.value;
-    const nickname = e.target.elements.nickname.value;
+  
 
-    const dynamicPath = `chat/chatRoomId=${chatRoomId}?nickname=${nickname}`;
+  // 구독 관련 코드 잠시 useEffect관련정보처리 위해 꺼놈
+  // const eventSource = new EventSource(`https://i10a701.p.ssafy.io/api/message/alert/subscribe/${userId}`);
 
-    navigate(dynamicPath);
-  };
+  // eventSource.onmessage = function (event) {
+  //   const eventData = JSON.parse(event.data);
+  //   // 서버에서 전송한 데이터를 처리
+  //   console.log('Received data:', eventData.message);
+  // };
 
-  const eventSource = new EventSource(`https://i10a701.p.ssafy.io/api/message/alert/subscribe/${userId}`);
-    
-    eventSource.onmessage = function (event) {
-        const eventData = JSON.parse(event.data);
-        // 서버에서 전송한 데이터를 처리
-        console.log('Received data:', eventData.message);
+  // eventSource.onerror = function (error) {
+  //   console.error('Error with SSE connection:', error);
+  //   eventSource.close();  // 에러 발생 시 연결 종료
+  // };
+
+  useEffect(() => {
+    // SockJS와 Stomp 설정
+    const socket = new SockJS(sockjsEndpoint);
+    const stompClient = new Client({
+      webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
+      onConnect: () => {
+        console.log('Connected');
+
+        // 서버로부터 메시지를 받을 구독 설정
+        stompClient.subscribe('/sub', message => {
+          console.log(`Received: ${message.body}`);
+        });
+
+        // 서버로 메시지 전송
+        stompClient.publish({
+          destination: '/pub',
+          body: 'First Message',
+        });
+      },
+    });
+
+    // 웹소켓 연결 활성화
+    stompClient.activate();
+
+    // 컴포넌트 언마운트 시 연결 종료
+    return () => {
+      stompClient.deactivate();
     };
-
-    eventSource.onerror = function (error) {
-        console.error('Error with SSE connection:', error);
-        eventSource.close();  // 에러 발생 시 연결 종료
-    };
-    useEffect(() => {
-      // SockJS와 Stomp 설정
-      const socket = new SockJS(sockjsEndpoint);
-      const stompClient = new Client({
-        webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
-        onConnect: () => {
-          console.log('Connected');
-          
-          // 서버로부터 메시지를 받을 구독 설정
-          stompClient.subscribe('/sub', message => {
-            console.log(`Received: ${message.body}`);
-          });
-  
-          // 서버로 메시지 전송
-          stompClient.publish({
-            destination: '/pub',
-            body: 'First Message',
-          });
-        },
-      });
-  
-      // 웹소켓 연결 활성화
-      stompClient.activate();
-  
-      // 컴포넌트 언마운트 시 연결 종료
-      return () => {
-        stompClient.deactivate();
-      };
-    }, []);
+  }, []);
 
   return (
     <div className="">
