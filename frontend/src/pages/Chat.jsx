e48c51b84bd2a3473cda9c5de56822ee0cb8d0f2
yhc-key <yhcho0712@gmail.com>
2024-02-05 17:26:58 +0900

    fix :rollback
    Change-Id: Ice01f2132078d52d1a9b11b62ecf2d96307c6fa4

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -22,116 +22,62 @@ const ChatPage = () => {
   const [tripId, setTripId] = useState("143");
   const [messages, setMessages] = useState([]);
 
-  // useEffect(() => {
-  //   // SockJS와 Stomp 설정
-  //   const socket = new WebSocket(sockJSEndPoint);
-    
-  //   const stompClient = new Client({
-  //     webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
-      
-  //     onConnect: (frame) => {
-  //       console.log('Connected', frame);
-
-  //       // 서버로부터 메시지를 받을 구독 설정
-  //       stompClient.subscribe('/sub/chat/room/room1', message => {
-  //         console.log(`Received: ${message.body}`);
-  //       });
-
-  //       // 서버로 메시지 전송
-  //       stompClient.publish({
-  //         destination: '/pub/chat/send',
-  //         body: JSON.stringify({ chatroomId : "room1", senderId : "조예진", content : "이제 해방이다"}),
-  //       });
-  //     },
-  //   });
-
-  //   // 웹소켓 연결 활성화
-  //   stompClient.activate();
-
-  //   // 컴포넌트 언마운트 시 연결 종료
-  //   return () => {
-  //     stompClient.deactivate();
-  //   };
-  // }, []);
-
   useEffect(() => {
-    // STOMP 클라이언트 설정
+    // SockJS와 Stomp 설정
+    const socket = new WebSocket(sockJSEndPoint);
+    
     const stompClient = new Client({
-      brokerURL: "wss://i10a701.p.ssafy.io/api/message/chat", // STOMP over WebSocket 연결 URL
-      connectHeaders: {
-        login: "guest", // RabbitMQ 로그인 정보 (필요한 경우)
-        passcode: "guest",
-      },
-      debug: function (str) {
-        console.log(str);
-      },
-      onConnect: () => {
-        console.log("Connected to STOMP broker");
+      webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
+      
+      onConnect: (frame) => {
+        console.log('Connected', frame);
 
-        // 메시지 수신을 위한 구독 설정
-        stompClient.subscribe("/sub/chat/room/room1", (message) => {
-          const newMessage = JSON.parse(message.body);
-          setMessages((prevMessages) => [...prevMessages, newMessage]);
+        // 서버로부터 메시지를 받을 구독 설정
+        stompClient.subscribe('/sub/chat/room/room1', message => {
           console.log(`Received: ${message.body}`);
         });
-      },
-      // 연결 실패 시 콜백
-      onStompError: (error) => {
-        console.error("Error connecting to STOMP broker:", error);
+
+        // 서버로 메시지 전송
+        stompClient.publish({
+          destination: '/pub/chat/send',
+          body: JSON.stringify({ chatroomId : "room1", senderId : "조예진", content : "이제 해방이다"}),
+        });
       },
     });
 
-    // STOMP 클라이언트 연결 활성화
+    // 웹소켓 연결 활성화
     stompClient.activate();
-    stompClientRef.current = stompClient
+
     // 컴포넌트 언마운트 시 연결 종료
     return () => {
       stompClient.deactivate();
     };
   }, []);
 
-  const sendMessage = (messageContent) => {
-    // 서버로 메시지 전송
-    if (stompClientRef.current.connected) {
-      stompClientRef.current.publish({
-        destination: "/pub/chat/send",
-        body: JSON.stringify({
-          chatroomId: "room1",
-          senderId: "조예진",
-          content: messageContent,
-        }),
-      });
+  
+
+  const clickHandler = async () => {
+    dummyData = { ...dummyData, tripId: tripId };
+    try {
+      const response = await axios.post(
+        "https://i10a701.p.ssafy.io/api/message/alert/attend",
+        dummyData,
+        {
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+        }
+      );
+      console.log("요청 보냈어요", response.data);
+    } catch (error) {
+      console.error("Error", error.response);
     }
   };
 
-  // 폼 제출 핸들러
   const submitHandler = (event) => {
     event.preventDefault();
-    sendMessage("이제 해방이다");
+    clickHandler();
   };
-
-  // const clickHandler = async () => {
-  //   dummyData = { ...dummyData, tripId: tripId };
-  //   try {
-  //     const response = await axios.post(
-  //       "https://i10a701.p.ssafy.io/api/message/alert/attend",
-  //       dummyData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "application/json;charset=UTF-8",
-  //         },
-  //       }
-  //     );
-  //     console.log("요청 보냈어요", response.data);
-  //   } catch (error) {
-  //     console.error("Error", error.response);
-  //   }
-  // };
-
-  // const submitHandler = (event) => {
-  //   event.preventDefault();
-  //   clickHandler();
-  // };
 
   return (
     <div className="">
