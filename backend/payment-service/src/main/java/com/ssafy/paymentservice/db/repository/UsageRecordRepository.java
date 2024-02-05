7fd9403c3f097c168de2e1a6a9c42697ec8a1a01
송윤재 <1218song@naver.com>
2024-02-05 13:48:53 +0900

    Fix: 마일리지 사용기록 기능 수정
    Change-Id: I8618a6e6b0f632e2bfac2626a39346d2c7ffd8a9

--- backend/payment-service/src/main/java/com/ssafy/paymentservice/db/repository/UsageRecordRepository.java
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/db/repository/UsageRecordRepository.java
@@ -1,10 +1,10 @@
 package com.ssafy.paymentservice.db.repository;
 
-import com.ssafy.paymentservice.db.entity.ChargeRecord;
+import com.ssafy.paymentservice.db.entity.UsageRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsageRecordRepository extends JpaRepository<ChargeRecord, String> {
+public interface UsageRecordRepository extends JpaRepository<UsageRecord, String> {
 
 }
