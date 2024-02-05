0cc4fb48e65bee30cd9755f0d903c3d8df9246f8
yhc-key <yhcho0712@gmail.com>
2024-02-05 17:24:14 +0900

    test: rabbitmq 4
    Change-Id: I4b4ec80ce4d721bcdefd43450dc9b475220662a5

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Button from "../ui/Button";
