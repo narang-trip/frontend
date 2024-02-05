c0427c14246de2a1e0c0045499f6a5e6a43c3928
송윤재 <1218song@naver.com>
2024-02-05 14:53:26 +0900

    Fix: db ddl update로 변경, 사용 기록 날짜 추가
    Change-Id: I517b55ee0ceb814ce2ff167f912685c0b6707cd9

--- backend/payment-service/src/main/java/com/ssafy/paymentservice/service/MileageService.java
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/service/MileageService.java
@@ -7,13 +7,16 @@ import com.ssafy.paymentservice.db.repository.UserMileageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class MileageService {
     private final UserMileageRepository userMileageRepository;
     private final UsageRecordRepository usageRecordRepository;
     public UsageRecord useMileage(String user_id, int price){
-        UserMileage userMileage = userMileageRepository.findById(user_id).get();
+        UserMileage userMileage = userMileageRepository.findById(user_id)
+                .orElseThrow(() -> new NoSuchElementException("User mileage not found..."));
         userMileage.setMileage(userMileage.getMileage() - price);
         userMileageRepository.save(userMileage);
         UsageRecord usageRecord = new UsageRecord(user_id, price, userMileage.getMileage());
