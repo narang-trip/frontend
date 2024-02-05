f091b932d0348ded5be42d400e452f970b9a50e0
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-06 00:43:04 +0900

    Feat: 시작 시간 끝 시간 계산 후 줄 긋기
    Change-Id: Id8630a5cc4da635b69cd305dd2d1238a906522d5

--- frontend/src/components/Planning/TimeLine.jsx
+++ frontend/src/components/Planning/TimeLine.jsx
@@ -5,18 +5,14 @@ const TimeLine = (props) => {
     Number(props.Time.startHour) -
     (Number(props.Time.endMinute) - Number(props.Time.startMinute)) / 30;
   console.log(lineCnt);
+  const m = lineCnt / 100;
 
   return (
-    <>
-      <div className="mt-8">
-        {[...Array(10)].map((_, index) => (
-          <div
-            key={index}
-            className={`border-b border-red-600 w-full mb-[${1}]`}
-          ></div>
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols h-full content-between">
+      {[...Array(lineCnt)].map((_, index) => (
+        <div key={index} className={`border-b border-red-600 w-full mt-8`}></div>
+      ))}
+    </div>
   );
 };
 
