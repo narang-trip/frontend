5404b64b2844ba1925883ebe66829854c102a8ce
송윤재 <1218song@naver.com>
2024-02-05 15:25:19 +0900

    Fix: mileage 없는 유저 신규 생성
    Change-Id: Ica503fcd3f07364ba3c8dcfcc61270d395dbed69

--- backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/UserMileage.java
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/UserMileage.java
@@ -3,12 +3,14 @@ package com.ssafy.paymentservice.db.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserMileage {
