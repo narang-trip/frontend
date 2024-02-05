3f0b3c58808002e78bc0ca31b293b308e1f50c1c
yhc-key <yhcho0712@gmail.com>
2024-02-05 13:28:37 +0900

    test : new test sse 1
    Change-Id: I84559a4d2abea0a06c7948048da3105c16ee832a

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -8,58 +8,58 @@ const userId = "조예진";
 const sockJSEndPoint = "https://i10a701.p.ssafy.io/api/message/chat"
 const stompEndpoint = "wss://i10a701.p.ssafy.io/api/message/chat";
 const ChatPage = () => {
-  // useEffect(() => {
-  //   const EventSource = EventSourcePolyfill || NativeEventSource;
-  //   const eventSource = new EventSource(
-  //     `https://i10a701.p.ssafy.io/api/message/alert/subscribe/${userId}`
-  //   );
-
-  //   eventSource.addEventListener("sse", (event) => {
-  //     const { data: receivedConnectData } = event;
-  //     console.log(receivedConnectData);
-  //     if (receivedConnectData === "SSE 연결이 완료되었습니다.") {
-  //       // "SSE connection has been completed."
-  //       console.log("SSE CONNECTED");
-  //     } else {
-  //       console.log(event);
-  //     }
-  //   });
-
-  //   return () => {
-  //     eventSource.close();
-  //     console.log("SSE CLOSED");
-  //   };
-  // }, []);
-
   useEffect(() => {
-    // SockJS와 Stomp 설정
-    const socket = new SockJS(sockJSEndPoint);
-    const stompClient = new Client({
-      webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
-      onConnect: (frame) => {
-        console.log('Connected', frame);
+    const EventSource = EventSourcePolyfill || NativeEventSource;
+    const eventSource = new EventSource(
+      `https://i10a701.p.ssafy.io/api/message/alert/subscribe/${userId}`
+    );
 
-        // 서버로부터 메시지를 받을 구독 설정
-        stompClient.subscribe('/sub/chat/room/room1', message => {
-          console.log(`Received: ${message.body}`);
-        });
-
-        // 서버로 메시지 전송
-        stompClient.publish({
-          destination: '/pub/chat/send',
-          body: JSON.stringify({ chatroomId : "room1", senderId : "조예진", content : "이제 해방이다"}),
-        });
-      },
+    eventSource.addEventListener("sse", (event) => {
+      const { data: receivedConnectData } = event;
+      console.log(receivedConnectData);
+      if (receivedConnectData === "SSE 연결이 완료되었습니다.") {
+        // "SSE connection has been completed."
+        console.log("SSE CONNECTED");
+      } else {
+        console.log(event);
+      }
     });
 
-    // 웹소켓 연결 활성화
-    stompClient.activate();
-
-    // 컴포넌트 언마운트 시 연결 종료
     return () => {
-      stompClient.deactivate();
+      eventSource.close();
+      console.log("SSE CLOSED");
     };
   }, []);
+
+  // useEffect(() => {
+  //   // SockJS와 Stomp 설정
+  //   const socket = new SockJS(sockJSEndPoint);
+  //   const stompClient = new Client({
+  //     webSocketFactory: () => socket, // SockJS 인스턴스를 사용하여 웹소켓 연결
+  //     onConnect: (frame) => {
+  //       console.log('Connected', frame);
+
+  //       // 서버로부터 메시지를 받을 구독 설정
+  //       stompClient.subscribe('/sub/chat/room/room1', message => {
+  //         console.log(`Received: ${message.body}`);
+  //       });
+
+  //       // 서버로 메시지 전송
+  //       stompClient.publish({
+  //         destination: '/pub/chat/send',
+  //         body: JSON.stringify({ chatroomId : "room1", senderId : "조예진", content : "이제 해방이다"}),
+  //       });
+  //     },
+  //   });
+
+  //   // 웹소켓 연결 활성화
+  //   stompClient.activate();
+
+  //   // 컴포넌트 언마운트 시 연결 종료
+  //   return () => {
+  //     stompClient.deactivate();
+  //   };
+  // }, []);
 
 
 
