c4c259271e5c381fcdfb81b71eebc31ef5b7ddbe
yhc-key <yhcho0712@gmail.com>
2024-02-05 11:30:33 +0900

    test : chat test 38
    Change-Id: If7eb42106616d8614e4e173f1962ed5336506cd7

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -39,15 +39,15 @@ const ChatPage = () => {
         console.log('Connected', frame);
 
         // 서버로부터 메시지를 받을 구독 설정
-        stompClient.subscribe('/api/message/sub/chat/room/room1', message => {
-          console.log(`Received: ${message.body}`);
-        });
+        // stompClient.subscribe('/api/message/sub/chat/room/room1', message => {
+        //   console.log(`Received: ${message.body}`);
+        // });
 
-        // 서버로 메시지 전송
-        stompClient.publish({
-          destination: '/api/message/pub/chat/send',
-          body: { chatRoomId : 1, senderId : "조예진", content : "이제 해방이다"},
-        });
+        // // 서버로 메시지 전송
+        // stompClient.publish({
+        //   destination: '/api/message/pub/chat/send',
+        //   body: { chatRoomId : 1, senderId : "조예진", content : "이제 해방이다"},
+        // });
       },
     });
 
