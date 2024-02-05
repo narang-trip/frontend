0df6a8c08d8b63ab4d002ac4d252c8d9b60be0a2
se2develop <seahee0130@gmail.com>
2024-02-05 17:43:55 +0900

    Feat: 모집글 작성 기능
    Change-Id: Iad7c5bf9cad781db5b94d4efd3fdf966fa099b88

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
         element: <TripRegisterPage />,
       },
       {
-        path: "/detail",
+        path: "/detail/:tripId",
         element: <TripDetailPage />,
       },
       {
--- frontend/src/components/modals/LeaveUserModal.jsx
+++ frontend/src/components/modals/LeaveUserModal.jsx
@@ -6,7 +6,7 @@ const LeaveUserModal = (props) => {
 
   return (
     <div
-      className="fixed top-0 bottom-0 left-0 right-0 z-0 flex items-center justify-center bg-gray-500 bg-opacity-70"
+      className="fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-gray-500 bg-opacity-70"
       onClick={props.onClose}
       ref={modalBG}
     >
