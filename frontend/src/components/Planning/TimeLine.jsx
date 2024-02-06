cacdfda86cbdf99a4cad03757a30bf64f15f99fa
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-06 10:15:14 +0900

    Test: 임시저장?
    Change-Id: I9a6fd081d33a6476a2796005629b60de20ccba61

--- frontend/src/components/Planning/TimeLine.jsx
+++ frontend/src/components/Planning/TimeLine.jsx
@@ -5,12 +5,14 @@ const TimeLine = (props) => {
     Number(props.Time.startHour) -
     (Number(props.Time.endMinute) - Number(props.Time.startMinute)) / 30;
   console.log(lineCnt);
-  const m = lineCnt / 100;
 
   return (
     <div className="grid grid-cols h-full content-between">
       {[...Array(lineCnt)].map((_, index) => (
-        <div key={index} className={`border-b border-red-600 w-full mt-8`}></div>
+        <div
+          key={index}
+          className={`border-b border-red-600 w-full mt-8 mb-4`}
+        ></div>
       ))}
     </div>
   );
