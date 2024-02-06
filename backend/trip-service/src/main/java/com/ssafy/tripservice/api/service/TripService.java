1e1cf6dfd4c4a541017bbcb479640581f579689c
김영섭 <kimyoungseop33@gmail.com>
2024-02-06 10:29:49 +0900

    update trip-service
    Change-Id: Ie07f275ec000994129891c29833c760a5c8b1461

--- backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
@@ -1,22 +1,32 @@
 package com.ssafy.tripservice.api.service;
 
 import com.ssafy.tripservice.api.request.TripRequest;
+import com.ssafy.tripservice.api.request.UserRequest;
 import com.ssafy.tripservice.api.response.TripResponse;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
 public interface TripService {
     /*
         Trip 생성
      */
-    Optional<TripResponse> createTrip(TripRequest tripRequest);
+    Optional<TripResponse> createTrip(TripRequest tripRequest, MultipartFile tripImg);
     /*
         현재 기준 참가 가능한 Trip
      */
-    List<TripResponse> getAvailableTrips(LocalDateTime currentTime);
+    List<TripResponse> getAvailableTrips();
 
     Optional<TripResponse> getTripById(UUID tripId);
+
+    Optional<Integer> getTripParticipantsSize(UUID tripId);
+
+    Optional<TripResponse> joinTrip(UserRequest userRequest);
+
+    Optional<TripResponse> leaveTrip(UserRequest userRequest);
 }
