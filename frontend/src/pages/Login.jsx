d61b79d0d257336a6f1d42c4a1d9f4914ce6ff07
yhc-key <yhcho0712@gmail.com>
2024-02-06 18:31:18 +0900

    fix : fix bug axios.post in login
    Change-Id: I37341308f14ce2cfb5c8ef346f28ac98ec86397f

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,21 +13,21 @@ const Login = () => {
     const code = params.get("code");
     console.log(code);
     console.log("test");
-   (async () => {
-    try {
-      const res = await axios.post(
-        "https://i10a701.p.ssafy.io/api/user/login/oauth/kakao",
-        { code: code }
-      );
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-    dispatch(authAction.Login(code));
-  })();
-})
+    (async () => {
+      try {
+        const res = await axios.post(
+          `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=${code}`,
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+      dispatch(authAction.Login(code));
+    })();
+  });
 
   return <>뭔가 뭔가 무언가의 페이지</>;
 };
 
 export default Login;
+
