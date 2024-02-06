e959f2ffffbdfae8eea7114e6473904871929719
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-06 16:08:01 +0900

    Test: 로그인 테스트
    Change-Id: I0472d7594457efe83f2d9e6c48fed1cc6b8cfff1

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,19 +2,32 @@ import { useDispatch } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { authAction } from "../store/auth-slice";
 import { useEffect } from "react";
+import axios from "axios";
 
 const Login = () => {
   const params = new URL(document.URL).searchParams;
   const code = params.get("code");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  console.log(code);
+
+  try {
+    const res = axios.post("api/user/login/oauth/kakao", {
+      authorizationCode: code,
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+
   dispatch(authAction.Login(code));
 
   console.log("test");
 
   useEffect(() => {
     console.log("여기 왔다감");
-    navigate("/");
+    // navigate("/");
   });
 
   return <>뭔가 뭔가 무언가의 페이지</>;
