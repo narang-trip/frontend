711b6fddc6ccaaba1ae3b70cbcb63d875fe344f2
yhc-key <yhcho0712@gmail.com>
2024-02-06 20:26:02 +0900

    test : login test
    Change-Id: I77f0b96fd0f8585b56b71c470dbcc43a85fe8acc

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,20 +5,32 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function HomePage() {
-  useEffect(() => {
-    getUser();
-  });
+  // useEffect(() => {
+  //   getUser();
+  // });
 
-  const getUser = () => {
-    axios
-      .get("/api/user/get")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const getUser = () => {
+  //   axios
+  //     .get("/api/user/get")
+  //     .then((response) => {
+  //       console.log(response);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+  (async () => {
+    try {
+      const res = await axios.post(
+        `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=355`
+      );
+      console.log(res.data);
+      // dispatch(authAction.Login(code));
+
+    } catch (error) {
+      console.log(error);
+    }
+  })();
 
   return (
     <div>
