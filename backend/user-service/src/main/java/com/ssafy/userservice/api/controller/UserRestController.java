7d8e2c1ecc463dab096a5e3509d92917741b859e
조예진 <cyj2825@naver.com>
2024-02-06 10:26:35 +0900

    Refactor: API 코드 수정 및 위치 옮기기
    Change-Id: I2fc0029c5adabca009f59defc5ac897dcd6f2241

--- backend/user-service/src/main/java/com/ssafy/userservice/api/controller/UserRestController.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/api/controller/UserRestController.java
@@ -1,9 +1,11 @@
 package com.ssafy.userservice.api.controller;
 
+import com.ssafy.userservice.api.request.UserInfoRequest;
 import com.ssafy.userservice.api.service.UserService;
 import com.ssafy.userservice.db.entity.PrincipalDetails;
 import com.ssafy.userservice.db.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +29,23 @@ public class UserRestController {
     public User getTest() {
         User user = userService.getTest("1").get();
         return user; // 로그인 성공시 uuid 리턴
+    }
+
+    // User 탈퇴
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable String id) {
+        return userService.deleteUser(id);
+    }
+
+    // User 정보 조회
+    @GetMapping("/profile/{id}")
+    public ResponseEntity<User> getUser(@PathVariable String id){
+        return userService.getUser(id);
+    }
+
+    // User 정보 수정
+    @PatchMapping("/profile/{id}")
+    public ResponseEntity<?> patchUser(@PathVariable String id, @RequestBody UserInfoRequest userInfoRequest){
+        return userService.patchUser(id, userInfoRequest);
     }
 }
