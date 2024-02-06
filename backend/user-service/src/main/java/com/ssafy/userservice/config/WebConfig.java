9f5538082feab42e1191336cee261234ddb291b8
송윤재 <1218song@naver.com>
2024-02-06 09:31:12 +0900

    Feat: CORS 허용을 위한 Config 작성
    Change-Id: I4750477b8e8a09b473e742cc101da690426a1b89

---
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 또는 특정 도메인을 지정
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .allowCredentials(true);
+    }
+}
