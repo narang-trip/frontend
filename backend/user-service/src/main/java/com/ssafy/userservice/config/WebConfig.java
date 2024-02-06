048be27a9759a96be593a269aeeaf93362b984e5
송윤재 <1218song@naver.com>
2024-02-06 10:10:06 +0900

    Fix: Allow Origins 변경
    Change-Id: Ia366f8cbcec103bd9920f459179dc45a609be37d

--- backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 또는 특정 도메인을 지정
+                .allowedOrigins("https://i10a701.p.ssafy.io") // 또는 특정 도메인을 지정
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type")
                 .allowCredentials(true);
