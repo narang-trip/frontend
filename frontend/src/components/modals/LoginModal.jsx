dde4d3048f70d7aad0395bd4c183a4faee5990fe
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-06 14:57:03 +0900

    Test: 로그인 테스트
    Change-Id: If209410f7dc3d10fe7ef822b8656ef4df30734fe

--- frontend/src/components/modals/LoginModal.jsx
+++ frontend/src/components/modals/LoginModal.jsx
@@ -7,50 +7,6 @@ const LoginModal = (props) => {
     "https://i10a701.p.ssafy.io/api/user/oauth2/authorization/kakao";
   const naverLoginURL = "https://i10a701.p.ssafy.io/oauth2/authorization/naver";
 
-  // const kakaoLogin = () => {
-  //   window.open(kakaoLoginURL);
-  // };
-
-  // const naverLogin = () => {
-  //   window.open(naverLoginURL);
-  // };
-
-  // axios.defaults.withCredentials = true;
-
-  // const params = new URL(document.URL).searchParams;
-  // const code = params.get("code");
-
-  // const kakaoLogin = async () => {
-  //   try {
-  //     const res = await axios.get(kakaoLoginURL);
-  //     console.log(res);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  const kakaoLogin = () => {
-    axios
-      .get(kakaoLoginURL)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  // const naverLogin = () => {
-  //   axios
-  //     .get(naverLoginURL)
-  //     .then((response) => {
-  //       console.log(response);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // };
-
   return (
     <div
       className="fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-gray-500 bg-opacity-70"
@@ -69,13 +25,13 @@ const LoginModal = (props) => {
           </button>
           <h3 className="">로 그 인</h3>
           <div className="flex flex-col">
-            {/* <a href={kakaoLoginURL}> */}
-            <img
-              className="object-cover h-16 w-36 rounded-xl"
-              src="assets/kakao_login.png"
-              onClick={kakaoLogin}
-            />
-            {/* </a> */}
+            <a href={kakaoLoginURL}>
+              <img
+                className="object-cover h-16 w-36 rounded-xl"
+                src="assets/kakao_login.png"
+                // onClick={kakaoLogin}
+              />
+            </a>
             <button>
               <img
                 className="object-cover h-16 w-36 rounded-xl"
