ba87c0911dcc3e377b641793d3e0a1c4fc3804bc
yhc-key <yhcho0712@gmail.com>
2024-02-06 18:44:55 +0900

    fix : bug login ing...
    Change-Id: I610d0acf1b4862405d927218fdcd8e5c997cf2cb

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,34 +13,19 @@ const Login = () => {
     const code = params.get("code");
     console.log(code);
     console.log("test");
-<<<<<<< HEAD
     (async () => {
       try {
         const res = await axios.post(
-          `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=${code}`,
+          `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=${code}`
         );
-        console.log(res);
+        console.log(res.data);
+    dispatch(authAction.Login(code));
+
       } catch (error) {
         console.log(error);
       }
-      dispatch(authAction.Login(code));
     })();
   });
-=======
-   (async () => {
-    try {
-      const res = await axios.post(
-        "https://i10a701.p.ssafy.io/api/user/login/oauth/kakao",
-        { provider: code }
-      );
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-    dispatch(authAction.Login(code));
-  })();
-})
->>>>>>> f304613d987e3f0bd13d26fbdc51a5d4034def38
 
   return <>뭔가 뭔가 무언가의 페이지</>;
 };
