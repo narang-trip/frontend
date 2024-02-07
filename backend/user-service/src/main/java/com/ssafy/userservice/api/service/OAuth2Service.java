5c9903b588be1ff7e6f967ffa4b19f9f881124e2
송윤재 <1218song@naver.com>
2024-02-07 12:32:47 +0900

    Fix: OAuth2Service 수정
    Change-Id: Id702e82b5c180afe3cd653f3e5c5156f3386427b

--- backend/user-service/src/main/java/com/ssafy/userservice/api/service/OAuth2Service.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/api/service/OAuth2Service.java
@@ -1,5 +1,6 @@
 package com.ssafy.userservice.api.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.beans.factory.annotation.Value;
 
+@Slf4j
 @Service
 public class OAuth2Service {
 
@@ -22,6 +24,9 @@ public class OAuth2Service {
     @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
     private String kakaoAuthorizationUri;
 
+    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
+    private String kakaoTokenUri;
+
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     public OAuth2Service(ClientRegistrationRepository clientRegistrationRepository) {
@@ -30,7 +35,7 @@ public class OAuth2Service {
 
     public String getAuthorizationUrl(String registrationId) {
 //        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId(registrationId);
-        System.out.println("getAuthorizationUrl 함수 호출============");
+        log.info("getAuthorizationUrl 함수 호출============");
         ClientRegistration clientRegistration = ClientRegistration
                 .withRegistrationId(registrationId)
                 .clientId(kakaoClientId)
@@ -39,16 +44,17 @@ public class OAuth2Service {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .scope("profile", "email")
                 .authorizationUri(kakaoAuthorizationUri)
+                .tokenUri(kakaoTokenUri)
                 .build();
-        System.out.println(clientRegistration.toString());
+        log.info(clientRegistration.toString());
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(clientRegistration.getProviderDetails().getAuthorizationUri())
                 .queryParam("client_id", clientRegistration.getClientId())
                 .queryParam("redirect_uri", clientRegistration.getRedirectUri())
                 .queryParam("response_type", "code")
                 .queryParam("scope", String.join(" ", clientRegistration.getScopes()));
-        System.out.println(builder.toString());
-        System.out.println(builder.toUriString());
+        log.info(builder.toString());
+        log.info(builder.toUriString());
         return builder.toUriString();
     }
 }
