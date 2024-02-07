1739ef94eeaca6d1b644643455cfd4662bc93dc1
송윤재 <1218song@naver.com>
2024-02-07 10:19:54 +0900

    Fix: 엔드포인트 변경
    Change-Id: Ibf40a6b341a248ace4106e50c7a4de47dc20f306

--- backend/user-service/src/main/java/com/ssafy/userservice/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/SecurityConfig.java
@@ -55,9 +55,9 @@ public class SecurityConfig {
                 .oauth2Login(oauth2Login -> oauth2Login
                         .successHandler(oAuth2LoginSuccessHandler)
                         .failureHandler(oAuth2LoginFailureHandler)
-//                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-//                                .userService(customOAuth2UserService)
-//                        )
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)
+                        )
     );
 
         http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
