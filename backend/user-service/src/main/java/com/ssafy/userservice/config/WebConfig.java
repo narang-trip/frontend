83cff8696bd9b1da0c42545a9bc4e7670ae97746
송윤재 <1218song@naver.com>
2024-02-07 14:14:39 +0900

    Test: test
    Change-Id: I83847184629d93542153c6601dd45dea1683e9ff

--- backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
@@ -16,5 +16,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .exposedHeaders("Authorization", "Authorization-refresh");
 //                .maxAge(3600);
+        // OAuth2 인증 엔드포인트에 대한 매핑 추가
+        registry.addMapping("/oauth/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization", "Authorization-refresh");
     }
+
+
 }
