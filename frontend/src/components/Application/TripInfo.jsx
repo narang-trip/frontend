a02f914f0c37e1c235f237de3e71feb20ac149bb
se2develop <seahee0130@gmail.com>
2024-02-07 15:03:43 +0900

    Feat: 모집 검색 무한스크롤 수정, 신청 현황 DB 연결
    Change-Id: Id61c1de65f30702a1130bc6fa1204e6f7728c757

--- frontend/src/components/Application/TripInfo.jsx
+++ frontend/src/components/Application/TripInfo.jsx
@@ -1,24 +1,30 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { SlCalender, SlLocationPin, SlPeople, SlBadge } from "react-icons/sl";
 import { RiArrowDropDownLine } from "react-icons/ri";
-import ReceiveRequestsInfo from "./ReceiveRequestsInfo";
 
-export default function TripInfo() {
+import ReceiveRequestsInfo from "./ReceiveRequestsInfo";
+import DateFormatter from "../DateFormatter";
+import ReceivedList from "./ReceivedList";
+
+export default function TripInfo({ tripData }) {
   // 내가 작성한 글 목록 -> 드롭다운 버튼으로 신청자 목록 확인 가능하도록
 
   // true면 해당 글의 신청목록 보여주고, false면 닫음
   const [viewOpen, setViewOpen] = useState(false);
 
-  const [positions, setPositions] = useState([
-    "역할1",
-    "역할2",
-    "역할3",
-    "역할4",
-  ]);
+  const [departureDate, setDepartureDate] = useState(null);
+  const [returnDate, setReturnDate] = useState(null);
 
+  // 신청자 목록 확인 가능하도록
   const handleViewClick = () => {
     setViewOpen(!viewOpen);
   };
+
+  useEffect(() => {
+    // 날짜 포맷 설정
+    setDepartureDate(DateFormatter({ dateString: tripData.departureDate }));
+    setReturnDate(DateFormatter({ dateString: tripData.returnDate }));
+  }, [tripData]);
 
   return (
     <Fragment>
@@ -34,31 +40,35 @@ export default function TripInfo() {
               </div>
               <div className="col-span-2">
                 <p className="my-1.5 text-sm font-bold text-center">
-                  2월 중순부터 2박 3일로 같이 런던 여행 가실 분
+                  {tripData.tripName}
                 </p>
                 <div className="flex flex-row items-center my-1.5 text-sm">
                   <SlCalender className="mx-3 " size="24" />
-                  <div> 2024.02.01 ~ 2024.02.06 </div>
+                  <div>
+                    {" "}
+                    {departureDate} ~ {returnDate}{" "}
+                  </div>
                 </div>
                 <div className="flex flex-row items-center my-1.5 text-sm">
                   <SlLocationPin className="mx-3 " size="24" />
-                  <div>유럽, 영국, 런던</div>
+                  <div> {tripData.destination}</div>
                 </div>
                 <div className="flex flex-row items-center my-1.5 text-sm">
                   <SlPeople className="mx-3" size="24" />
-                  <div> 2 / 4 </div>
+                  <div> 2 / {tripData.tripParticipantsSize} </div>
                 </div>
                 <div className="flex flex-row items-center my-1.5 text-sm">
                   <SlBadge className="mx-3 " size="24" />
                   <div className="flex flex-wrap justify-between">
-                    {positions.map((position, index) => (
-                      <span
-                        key={index}
-                        className="inline-flex items-center px-2 py-1 m-0.5 text-sm font-medium rounded-full bg-stone-100 ring-1 ring-inset ring-stone-500"
-                      >
-                        {position}
-                      </span>
-                    ))}
+                    {tripData.tripRoles &&
+                      tripData.tripRoles.map((role, index) => (
+                        <span
+                          key={index}
+                          className="inline-flex items-center px-2 py-1 m-0.5 text-sm font-medium rounded-full text-neutral-700 bg-stone-100 ring-1 ring-inset ring-stone-500"
+                        >
+                          {role}
+                        </span>
+                      ))}
                   </div>
                 </div>
               </div>
@@ -66,10 +76,10 @@ export default function TripInfo() {
           </button>
           <div
             className={`overflow-hidden transition-all duration-500 ease-in-out ${
-              viewOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+              viewOpen ? " opacity-100" : "max-h-0 opacity-0"
             }`}
           >
-            {viewOpen && <ReceiveRequestsInfo />}
+            {viewOpen && <ReceivedList tripId={tripData.tripId} />}
           </div>
         </ul>
       </div>
