2bfcda100c81794fb7ed9570d96b265e67209573
송윤재 <1218song@naver.com>
2024-02-07 17:40:48 +0900

    Fix: controller 수정
    Change-Id: I09cc607c5dc153939c56c044db5d8dbb6e0b563a

--- backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
+++ backend/user-service/src/main/java/com/ssafy/userservice/config/WebConfig.java
@@ -13,20 +13,20 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/user/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-//                .allowCredentials(true)
+                .allowCredentials(true)
                 .exposedHeaders("Authorization", "Authorization-refresh");
 //                .maxAge(3600);
         // OAuth2 인증 엔드포인트에 대한 매핑 추가
         registry.addMapping("/oauth/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-//                .allowCredentials(true)
+                .allowCredentials(true)
                 .exposedHeaders("Authorization", "Authorization-refresh");
         // 추가: Kakao 계정 로그인에 대한 CORS 설정
         registry.addMapping("/api/user/oauth2/authorization/kakao")
                 .allowedOrigins("https://i10a701.p.ssafy.io") // 실제 도메인으로 변경
                 .allowedMethods("GET", "POST")
-//                .allowCredentials(true)
+                .allowCredentials(true)
                 .exposedHeaders("Authorization", "Authorization-refresh");
     }
 }
