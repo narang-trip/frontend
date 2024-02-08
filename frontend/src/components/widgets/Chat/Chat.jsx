e7920605f50dc1e5509f272f1028342c22d35dd0
yhc-key <yhcho0712@gmail.com>
2024-02-08 09:30:16 +0900

    test : chatroom test
    Change-Id: I82a7d095f139364247789532daa20dada8ac9022

--- frontend/src/components/widgets/Chat/Chat.jsx
+++ frontend/src/components/widgets/Chat/Chat.jsx
@@ -3,7 +3,7 @@ import { Client } from "@stomp/stompjs";
 import axios from "axios";
 import Button from "../../../ui/Button";
 import { useSelector } from "react-redux";
-import { MdArrowBack } from "react-icons/md"
+import { MdArrowBack } from "react-icons/md";
 
 const stompEndpoint = "wss://i10a701.p.ssafy.io/api/message/chat";
 const Chat = ({ chatroomId }) => {
@@ -20,18 +20,18 @@ const Chat = ({ chatroomId }) => {
       stompClient.current = new Client({
         webSocketFactory: () => socket,
         onConnect: (frame) => {
-          console.log("Connected", frame);
-
           // 구독 설정
           stompClient.current.subscribe(
             `/sub/chat/room/${chatroomId}`,
             (message) => {
               console.log(`Received: ${message.body}`);
+              console.log(message.body.sendTime)
+              console.log(message.body.content)
               const chat = {
-                userId : userId,
-                sendTime : message.body.sendTime,
-                content : message.body.content
-              }
+                userId: userId,
+                sendTime: message.body.sendTime,
+                content: message.body.content,
+              };
               setChats((prevChats) => [...prevChats, chat]);
             }
           );
@@ -58,7 +58,6 @@ const Chat = ({ chatroomId }) => {
     };
   }, [chatroomId]);
 
-
   const getChatList = async (chatroomId, page) => {
     try {
       const res = await axios.get(
@@ -66,8 +65,7 @@ const Chat = ({ chatroomId }) => {
       );
       const chatList = [...res.data.chatList].reverse(); //순서 맞추기 위해서 뒤집어서 넣어주기
       setChats(chatList);
-      
-      scrollToBottom();
+
     } catch (error) {
       console.error(error);
     }
@@ -92,7 +90,7 @@ const Chat = ({ chatroomId }) => {
         destination: "/pub/chat/send",
         body: JSON.stringify({
           chatroomId: chatroomId,
-          senderId: userId, 
+          senderId: userId,
           content: msg,
         }),
       });
@@ -100,20 +98,22 @@ const Chat = ({ chatroomId }) => {
     }
   };
 
-  const navigateBack = () => {
-
-  }
+  const navigateBack = () => {};
   return (
     <Fragment>
       <button onClick={navigateBack} className="p-2 fixed top-0 left-0 z-10">
         <MdArrowBack className="text-2xl" /> {/* 아이콘 사이즈 조정 */}
       </button>
-      <div className="pb-10 overflow-y-auto w-full pr-4 relative" ref={chatListRef}>
+      <div
+        className="pb-10 overflow-y-auto w-full pr-4 relative"
+        ref={chatListRef}
+      >
         {chats.map((chat) => {
           const date = new Date(chat.sendTime);
           const formattedDate = `${date.getHours()}시 ${date.getMinutes()}분`;
           let messageBoxClass = "w-full flex m-3 items-end";
-          let messageClass = "text-sm rounded-lg border p-2 max-w-[80%] break-words";
+          let messageClass =
+            "text-sm rounded-lg border p-2 max-w-[80%] break-words";
           if (chat.userId === "관리자") {
             messageBoxClass += " justify-center"; // 관리자 일 땐 가운데
             messageClass += " bg-gray-200 w-[80%] border-gray-200";
@@ -121,25 +121,26 @@ const Chat = ({ chatroomId }) => {
               <div key={chat.chatId} className={messageBoxClass}>
                 <div className={messageClass}>{chat.content}</div>
               </div>
-            )
+            );
           } else if (chat.userId === userId) {
             messageBoxClass += " justify-end "; // 본인인 건 오른쪽
             messageClass += " bg-yellow-200 border-yellow-300";
             return (
               <div key={chat.chatId} className={messageBoxClass}>
-                <div className="text-xs mr-1">{formattedDate}</div><div className={messageClass}>{chat.content}</div>
+                <div className="text-xs mr-1">{formattedDate}</div>
+                <div className={messageClass}>{chat.content}</div>
               </div>
-            )
+            );
           } else {
             messageBoxClass += " justify-start"; // 상대방은 왼쪽
             messageClass += " bg-white";
             return (
               <div key={chat.chatId} className={messageBoxClass}>
-                <div className={messageClass}>{chat.content}</div><div className="text-xs ml-1">{formattedDate}</div>
+                <div className={messageClass}>{chat.content}</div>
+                <div className="text-xs ml-1">{formattedDate}</div>
               </div>
-            )
+            );
           }
-
         })}
       </div>
 
@@ -153,7 +154,6 @@ const Chat = ({ chatroomId }) => {
           value={msg}
           onChange={(e) => setMsg(e.target.value)}
           className="flex-grow p-2 mr-2 border rounded-lg focus:outline-none"
-
         />
         <Button type="submit">전송</Button>
       </form>
