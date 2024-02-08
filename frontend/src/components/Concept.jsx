13a35ea77b19ab0319ec3c9c3cda23515e642eb9
yhc-key <yhcho0712@gmail.com>
2024-02-08 09:41:08 +0900

    fix : delete practice, chatPage
    Change-Id: I803ec522e43d2cf7698d7d994fdfcc9ce7864971

--- frontend/src/components/Concept.jsx
+++ frontend/src/components/Concept.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
-
-const conceptImage = [0, 1, 2, 3, 4, 5, 6];
+import Button from "../ui/Button";
+import { conceptList, conceptColorObject } from "../data/concepts";
+import { useSelector, useDispatch } from "react-redux";
+import { conceptActions } from "../store/conceptSlice";
 
 export default function Concept() {
-  const [concept, setConcept] = useState(0);
-  console.log(concept);
-  return <div className="flex">
-    {conceptImage.map((value) => {
-      return <img onClick={() => setConcept(value)} className="m-1 rounded-full" src={`concept/${value}.jpg`} key={value} width="12.5%" height="auto"/>
-    })}
-    
-  </div>
+  const dispatch = useDispatch();
+  const conceptColor = useSelector((state) => state.concept.color)
+  const clickHandler = (value) => {
+    dispatch(conceptActions.changeConcept({ concept: value }));
+    console.log(conceptColor);
+  };
+  
+  return (
+    <div className="flex justify-between m-3">
+      {conceptList.map((value) => {
+        const color = conceptColorObject[value];
+        return (
+          <Button
+            onClick={() => clickHandler(value)}
+            className={`m-1 w-20 min-w-20 h-20 rounded-lg ${color}} neon-red`}
+            key={value}
+          >
+            {value}
+          </Button>
+        );
+      })}
+    </div>
+  );
 }
