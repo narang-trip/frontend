b8b72193bb47f1de55597266599a2777363909b1
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-09 23:58:30 +0900

    Test: 카카오, 네이버 로그인 구분하기
    Change-Id: I41f02c3f662f56fb4e5d922ed2807716c6fb4b17

---
+++ frontend/src/pages/KakaoLogin.jsx
@@ -0,0 +1,59 @@
+import { useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
+import { authActions } from "../store/authSlice";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  let token;
+  useEffect(() => {
+    const params = new URL(document.URL).searchParams;
+    const code = params.get("code");
+    console.log(code);
+    console.log("test");
+    (async () => {
+      try {
+        const res = await axios.post(
+          `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=${code}`
+          // "https://i10a701.p.ssafy.io/api/user/login/oauth/kakao",
+          // {
+          //   code: code,
+          // }
+        );
+        console.log(res);
+        console.log(res.data);
+        token = res.data;
+        userId = "temp";
+        dispatch(authActions.Login({ token, userId }));
+        navigate("/");
+      } catch (error) {
+        console.log("Error during POST request:", error);
+      }
+    })();
+  });
+
+  const test = async () => {
+    console.log(test, token);
+    try {
+      const res = await axios.get(`https://i10a701.p.ssafy.io/api/user/getuser`, {
+        token: token,
+      });
+      console.log(res);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={test}>유저정보 가져오기</button>
+      <b />
+      뭔가 뭔가 무언가의 페이지 카카오로그인
+    </>
+  );
+};
+
+export default Login;
