68cc5142fb0a5310b851a440f516274533c99d5d
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-09 18:28:37 +0900

    Feat: 계획 저장 불러오기 테스트
    Change-Id: I9b970a437e5c9ca4c6a640a9660addb94ce13f73

--- frontend/src/store/scheduleSlice.js
+++ frontend/src/store/scheduleSlice.js
@@ -34,30 +34,24 @@ const scheduleSlice = createSlice({
   reducers: {
     // 일정 추가하기
     addSchedule: (state, action) => {
-      state.list[action.payload.day].splice(
-        action.payload.index,
-        0,
-        action.payload.schedule
-      );
+      state.list[action.payload.day].splice(action.payload.index, 0, action.payload.schedule);
     },
     // 일정 움직이기
     moveSchedule: (state, action) => {
-      const data = state.list[action.payload.start.day].splice(
-        action.payload.start.index,
-        1
-      );
-      state.list[action.payload.end.day].splice(
-        action.payload.end.index,
-        0,
-        ...data
-      );
+      const data = state.list[action.payload.start.day].splice(action.payload.start.index, 1);
+      state.list[action.payload.end.day].splice(action.payload.end.index, 0, ...data);
     },
     // 저장된 계획 불러오기
     setSchedule: (state, action) => {
+      console.log(action.payload);
+      state.time = action.payload.time;
+      state.blackHeight = action.payload.blackHeight;
+      state.list = action.payload.list;
       state = action.payload;
     },
     // 여행 계획표의 시간 설정
     setTime: (state, action) => {
+      console.log(action.payload);
       state.time = action.payload;
       state.time.lineCnt = action.payload.totalTime / 30 + 1;
       // 설정한 날 만큼 빈칸으로 채워진 일정 생성
@@ -74,13 +68,11 @@ const scheduleSlice = createSlice({
     },
     // 일정 체류시간 변경
     setScheduleTime: (state, action) => {
-      state.list[action.payload.day][action.payload.index].time =
-        action.payload.time;
+      state.list[action.payload.day][action.payload.index].time = action.payload.time;
     },
     // 일정 한마디
     setComment: (state, action) => {
-      state.list[action.payload.day][action.payload.index].comment =
-        action.payload.comment;
+      state.list[action.payload.day][action.payload.index].comment = action.payload.comment;
     },
   },
 });
