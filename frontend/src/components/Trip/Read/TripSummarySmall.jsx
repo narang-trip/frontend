0a26f984cdfc1141108ffff6388cfe34e0d2a29d
yhc-key <yhcho0712@gmail.com>
2024-02-10 01:08:56 +0900

    feat: mainPage css ing...
    Change-Id: I32403b610aa7bd0de45966784d0bd0cbaadaec07

--- frontend/src/components/Trip/Read/TripSummarySmall.jsx
+++ frontend/src/components/Trip/Read/TripSummarySmall.jsx
@@ -34,27 +34,27 @@ const TripSummarySmall = ({ trip }) => {
   };
 
   return (
-    <div className="w-full rounded-lg mb-5">
+    <div className="w-full rounded-lg h-[45%]">
       <button
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         onClick={handleViewClick}
-        className="px-3 py-4 mx-3 my-4 border flex align-top rounded-3xl bg-stone-200 border-stone-400"
+        className="px-3 py-4 mx-3 my-4 border flex align-top rounded-3xl bg-stone-200 border-stone-400 h-full"
       >
         <div className="grid grid-cols-2 gap-4">
           <div className="relative overflow-hidden group rounded-3xl col-span-1">
             <img
               src={`assets/airplain.jpg`}
               alt="tripThumbnail"
-              className="w-full h-[9rem] mb-2 transition-transform duration-300 ease-in-out transform scale-100 rounded-3xl group-hover:scale-125"
+              className="object-cover w-full h-full transition-transform duration-300 ease-in-out transform scale-100 rounded-3xl group-hover:scale-125 "
             />
             {isHovered && (
               <div className="absolute inset-0 flex flex-col items-end justify-end bg-black text-neutral-800 bg-opacity-20">
-                <div className="flex flex-row items-center my-3 mr-4 text-sm font-semibold">
-                  <SlPeople className="mx-3" size="16" />
-                  <p className="text-sm">2 / {trip.tripParticipantsSize} </p>
-                  <SlEye className="mx-3" size="16" />
-                  <p className="text-sm">{trip.viewCnt}</p>
+                <div className="flex flex-row items-center my-1 mr-2 text-xs font-semibold" style={{ whiteSpace: 'nowrap', minWidth: '100px' }}>
+                  <SlPeople className="mx-1" size="12" />
+                  <p className="text-xs">2 / {trip.tripParticipantsSize}</p>
+                  <SlEye className="mx-1" size="12" />
+                  <p className="text-xs">{trip.viewCnt}</p>
                 </div>
               </div>
             )}
@@ -67,7 +67,7 @@ const TripSummarySmall = ({ trip }) => {
               </p>
             </div>
             <div className="flex flex-row items-center my-3 text-sm">
-              <SlLocationPin className="mx-2 text-4xl"  />
+              <SlLocationPin className="mx-2 text-4xl" />
               <p className="text-xs">{trip.destination}</p>
             </div>
             <div className="flex flex-row items-center my-3 text-sm">
