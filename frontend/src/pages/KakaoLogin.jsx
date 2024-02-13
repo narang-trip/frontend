5f7a67db058366f8f341c8fa40afea09f7e21b53
yhc-key <yhcho0712@gmail.com>
2024-02-13 16:14:17 +0900

    feat: save userId in redux
    Change-Id: Ib4f61531befbdfd9c7782496b039ea88ce1aaecc

--- frontend/src/pages/KakaoLogin.jsx
+++ frontend/src/pages/KakaoLogin.jsx
@@ -7,48 +7,51 @@ import axios from "axios";
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  let token;
+  let auth = "";
+  let refreshAuth = "";
   useEffect(() => {
     const params = new URL(document.URL).searchParams;
     const code = params.get("code");
-    console.log(code);
-    console.log("test");
     (async () => {
       try {
         const res = await axios.post(
           `https://i10a701.p.ssafy.io/api/user/login/oauth/kakao?code=${code}`
         );
-        console.log(res);
-        console.log(res.headers.authorization);
-        dispatch(authActions.Login({ token: res.headers.authorization }));
+        // console.log(res);
+        auth = res.headers["authorization"];
+        refreshAuth = res.headers["authorization-refresh"];
+        try {
+          const userRes = await axios.get(
+            "https://i10a701.p.ssafy.io/api/user/getuser",
+            {
+              headers: {
+                Authorization: auth,
+                "Authorization-refresh": refreshAuth,
+              },
+            }
+          );
+
+          dispatch(
+            authActions.Login({
+              token: res.headers.authorization,
+              refreshToken: res.headers["authorization-refresh"],
+              userId: userRes.data,
+            })
+          );
+        } catch (error) {
+          console.error("유저받아오면서 문제생김 : ", error);
+        }
+
         navigate("/");
       } catch (error) {
         console.log("Error during POST request:", error);
       }
     })();
-  });
+  }, []);
 
-  const test = async () => {
-    console.log(test, token);
-    try {
-      const res = await axios.get(
-        `https://i10a701.p.ssafy.io/api/user/getuser`,
-        {
-          token: token,
-        }
-      );
-      console.log(res);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <>
-      <button onClick={test}>유저정보 가져오기</button>
-      <b />
-      뭔가 뭔가 무언가의 페이지 카카오로그인
     </>
   );
 };
