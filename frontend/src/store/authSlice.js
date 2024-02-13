f728f09a7bf71bc739ca5609dbee766301526d8f
yhc-key <yhcho0712@gmail.com>
2024-02-13 16:42:23 +0900

    test: login test7
    Change-Id: I03f632d652105969ad38c3cf496373abedb028fb

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -11,8 +11,14 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     Login: (state, action) => {
-      console.log(action.payload);
       state.isLogin = true; // 로그인
+      state.token = action.payload.token; // 이거 유효성 체크 조금이라도 넣어봅시다.
+      state.refreshToken = action.payload.refreshToken; 
+      state.userId = action.payload.userId;
+      window.sessionStorage.setItem("token", action.payload.token);
+      window.sessionStorage.setItem("refreshToken", action.payload.refreshToken);
+    },
+    Setting: (state, action) => {
       state.token = action.payload.token; // 이거 유효성 체크 조금이라도 넣어봅시다.
       state.refreshToken = action.payload.refreshToken; 
       state.userId = action.payload.userId;
