51c32c4e60fee94d6ceab3cfa18f35126eb1f6bb
1218song <1218song@naver.com>
2024-02-13 23:49:10 +0900

    Feat: Plan CRUD
    Change-Id: Ie395a397e2aad37203284bb87bd3421803e09ee7

--- backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/PlanService.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/PlanService.java
@@ -1,5 +1,17 @@
 package com.ssafy.tripservice.api.service;
 
 
+import com.ssafy.tripservice.api.request.PlanModifyRequest;
+import com.ssafy.tripservice.api.request.PlanRequest;
+import com.ssafy.tripservice.api.response.PlanResponse;
+import com.ssafy.tripservice.api.response.TripResponse;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PlanService {
+    Optional<PlanResponse> createPlan(PlanRequest planRequest);
+    Optional<PlanResponse> modifyPlan(PlanModifyRequest planModifyRequest);
+    Optional<PlanResponse> getPlanById(UUID planId);
+    boolean deletePlan(UUID planId);
 }
