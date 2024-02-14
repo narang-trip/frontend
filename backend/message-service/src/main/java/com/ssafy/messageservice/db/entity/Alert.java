6cb51adc49931c8105ddc0683440142ee461030b
송윤재 <1218song@naver.com>
2024-02-14 16:12:46 +0900

    Fix: position 데이터 타입 변경
    Change-Id: I64071a4b086e4ca2a3bfdddab916eb8b9041206e

--- backend/message-service/src/main/java/com/ssafy/messageservice/db/entity/Alert.java
+++ backend/message-service/src/main/java/com/ssafy/messageservice/db/entity/Alert.java
@@ -33,8 +33,8 @@ public class Alert {
     @Column(length = 50)
     private String receiverId;
 
-//    @Column
-//    private List<String> position;
+    @Column
+    private String position;
 
     @Column
     private String aspiration;
