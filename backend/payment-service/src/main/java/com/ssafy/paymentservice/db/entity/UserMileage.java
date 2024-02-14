b74bb20c435faf577dc85a9687d4024dc61e41ac
1218song <1218song@naver.com>
2024-02-14 21:06:19 +0900

    Fix: UserMileage 구조 변경
    Change-Id: I928a22d8aa1970846167653367f9329923dc7bf5

--- backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/UserMileage.java
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/UserMileage.java
@@ -3,19 +3,21 @@ package com.ssafy.paymentservice.db.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-public class UserMileage {
-    @Id
-    private String id;
+public class UserMileage extends BaseEntity{
+    @Column
+    private String userId;
     @Column
     private String encryptedMileage;
+    @Builder
+    public UserMileage(String userId, String encryptedMileage){
+        super();
+        this.userId = userId;
+        this.encryptedMileage = encryptedMileage;
+    }
 }
