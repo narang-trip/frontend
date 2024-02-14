949e7b420d80633674dfe68e89501499a1a786d2
rnqhstmd0219 <rnqhstmd0219@gmail.com>
2024-02-14 09:36:40 +0900

    Feat: 계획에서 삭제하기
    Change-Id: I2e0c0bbd5da4e48162c26f8d985dcdcc1989c3b0

--- frontend/src/components/modals/SavePlanModal.jsx
+++ frontend/src/components/modals/SavePlanModal.jsx
@@ -1,15 +1,18 @@
 import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { scheduleActions } from "../../store/scheduleSlice";
 import { useMemo } from "react";
 import { useEffect } from "react";
+import { IoMdClose } from "react-icons/io";
+
+import { scheduleActions } from "../../store/scheduleSlice";
 
 const SavePlanModal = (props) => {
   const modalBG = useRef(null);
   const state = useSelector((state) => state.schedule);
   const dispatch = useDispatch();
   const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
 
   const savePlan = async () => {
     console.log(title, window.btoa(encodeURIComponent(JSON.stringify(state))));
@@ -39,8 +42,12 @@ const SavePlanModal = (props) => {
     setTitle(state.title);
   }, [state.title]);
 
-  const onChange = (e) => {
+  const onChangeTitle = (e) => {
     setTitle(e.target.value);
+  };
+
+  const onChangeDesc = (e) => {
+    setDesc(e.target.value);
   };
 
   return (
@@ -55,19 +62,39 @@ const SavePlanModal = (props) => {
           e.stopPropagation();
         }}
       >
-        <button className="" onClick={props.onClose}>
-          x
-        </button>
-        <label>계획이름</label>
-        <input
-          type="text"
-          value={title}
-          onChange={onChange}
-          placeholder="..."
-        ></input>
-        <button className="" onClick={savePlan}>
-          저장하기
-        </button>
+        <div className="flex justify-end">
+          <button
+            className="mb-4 text-xl font-semibold hover:text-red-600"
+            onClick={props.onClose}
+          >
+            <IoMdClose />
+          </button>
+        </div>
+        <div className="flex flex-col justify-center gap-2 pb-4">
+          <div className="flex justify-center gap-4">
+            <label className="text-lg">계획이름 : </label>
+            <input
+              className="rounded-md py-1.5 text-gray-900 ring-2 ring-gray-400 focus:outline-none focus:ring-3 focus:ring-yellow-500 caret-transparent"
+              type="text"
+              value={title}
+              onChange={onChangeTitle}
+            />
+          </div>
+          <label className="text-lg">간단한 계획 소개</label>
+          <textarea
+            className="rounded-md py-1.5 text-gray-900 ring-2 ring-gray-400 focus:outline-none focus:ring-3 focus:ring-yellow-500 caret-transparent"
+            value={desc}
+            onChange={onChangeDesc}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="inline-flex items-center px-4 py-2 text-sm font-semibold text-yellow-700 rounded-md bg-yellow-300 ring-1 ring-inset ring-blue-700/10"
+            onClick={savePlan}
+          >
+            저장하기
+          </button>
+        </div>
       </div>
     </div>
   );
