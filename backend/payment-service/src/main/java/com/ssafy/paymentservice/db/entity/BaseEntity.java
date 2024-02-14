1bf0a516f7f873254772ea03f27b898803838510
김영섭 <kimyoungseop33@gmail.com>
2024-02-14 15:17:28 +0900

    Fix : Milage Usage Entity UUID Gen Type
    Change-Id: Ic7d8d6e4086b10fb1bc200662e11e9b6ccdf44cb

--- backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/BaseEntity.java
+++ backend/payment-service/src/main/java/com/ssafy/paymentservice/db/entity/BaseEntity.java
@@ -1,10 +1,15 @@
 package com.ssafy.paymentservice.db.entity;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.ValueGenerationType;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -13,6 +18,7 @@ import java.util.UUID;
 public class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     private UUID id;
 
     public BaseEntity() {
