7fc1525ea0b0750f211dc0d9fdfef677053af275
se2develop <seahee0130@gmail.com>
2024-02-14 10:28:08 +0900

    Feat: 신청현황 수정
    Change-Id: I7d51633e05ceb108d8bcd6a759b5128ac29c63c3

--- frontend/src/components/Trip/Read/TripParticipantsInfo.jsx
+++ frontend/src/components/Trip/Read/TripParticipantsInfo.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState, useEffect } from "react";
 import axios from "axios";
+import UserInfo from "../../Mypage/UserInfo";
 
 export default function TripParticipantsInfo({ participants, leaderId }) {
   const [usersInfo, setUsersInfo] = useState({});
@@ -45,7 +46,11 @@ export default function TripParticipantsInfo({ participants, leaderId }) {
           participants.map((participant, idx) => (
             <div
               key={idx}
-              className={`grid grid-rows-2 px-2 pt-1 mb-2  rounded-lg ${usersInfo[participant.participantId] === leaderId ? 'bg-amber-300 bg-opacity-20' : 'border border-neutral-200'}`}
+              className={`grid grid-rows-2 px-2 pt-1 mb-2  rounded-lg ${
+                participant.participantId === leaderId
+                  ? "bg-amber-300 bg-opacity-20"
+                  : "border border-neutral-200"
+              }`}
             >
               <div className="row-span-1 mx-2 my-2">
                 <div className="grid grid-cols-5">
