d1d7cea427586f65b4eba99ac46a41e0dcab7455
se2develop <seahee0130@gmail.com>
2024-02-14 10:43:22 +0900

    Feat: 로그인 여부 확인
    Change-Id: I4a40f0774a5e34f9a1cc63220be95d2b0e124ad3

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,7 +1,6 @@
 import { useState, Fragment, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import { ko } from "date-fns/locale";
-import { useSelector } from "react-redux";
 
 import MypageCalendar from "../components/Mypage/MypageCalendar";
 import UserInfo from "../components/Mypage/UserInfo";
@@ -10,7 +9,6 @@ import Mileage from "../components/Mypage/Mileage";
 import MyBadge from "../components/Mypage/MyBadge";
 
 const Mypage = () => {
-  const { isLogin } = useSelector((state) => state.auth);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
 
@@ -19,45 +17,38 @@ const Mypage = () => {
     setEndDate(range[1]);
   };
 
-
   return (
     <Fragment>
-      {isLogin ? (
-        <div className="grid w-full h-screen grid-cols-9 gap-5">
-          <div className="col-span-6">
-            <div className="w-full my-3">
-              <Fragment>
-                <p className="mt-6 text-xl font-bold">🛫나의 여행기록🛬</p>
-                <div className="flex items-center justify-center w-full">
-                  <span className="">날짜 선택</span>
-                  <DatePicker
-                    locale={ko}
-                    selectsRange={true}
-                    onChange={onChange}
-                    startDate={startDate}
-                    endDate={endDate}
-                    dateFormat="yy/MM/dd"
-                    todayButton="now"
-                    className="w-auto p-2 m-3 text-sm border rounded-sm border-neutral-300 text-neutral-700 placeholder:text-neutral-300"
-                  />
-                </div>
-                <SmallPlan dates={[startDate, endDate]} />
-              </Fragment>
-            </div>
-          </div>
-          <div className="col-span-3">
-            <div className="flex flex-col mt-10">
-              <UserInfo />
-              <Mileage />
-              <MyBadge />
-            </div>
+      <div className="grid w-full h-screen grid-cols-9 gap-5">
+        <div className="col-span-6">
+          <div className="w-full my-3">
+            <Fragment>
+              <p className="mt-6 text-xl font-bold">🛫나의 여행기록🛬</p>
+              <div className="flex items-center justify-center w-full">
+                <span className="">날짜 선택</span>
+                <DatePicker
+                  locale={ko}
+                  selectsRange={true}
+                  onChange={onChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  dateFormat="yy/MM/dd"
+                  todayButton="now"
+                  className="w-auto p-2 m-3 text-sm border rounded-sm border-neutral-300 text-neutral-700 placeholder:text-neutral-300"
+                />
+              </div>
+              <SmallPlan dates={[startDate, endDate]} />
+            </Fragment>
           </div>
         </div>
-      ) : (
-        <div className="flex items-start w-full h-full">
-        <div className="mx-auto mt-24 text-xl font-bold text-center ">로그인을 해주세요</div>
-        </div> 
-      )}
+        <div className="col-span-3">
+          <div className="flex flex-col mt-10">
+            <UserInfo />
+            <Mileage />
+            <MyBadge />
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
