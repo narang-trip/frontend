caf89d33f94f9f1018251b557f1f55084aa1f72f
yhc-key <yhcho0712@gmail.com>
2024-02-15 15:49:39 +0900

    refactor : dont use hook delete
    Change-Id: Ib66fce344587db312e4b1f1ebe03445aad9c9a6b

--- frontend/src/components/widgets/Widgets.jsx
+++ frontend/src/components/widgets/Widgets.jsx
@@ -1,6 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import ClipLoader from "react-spinners/ClipLoader";
 
 import CalendarCp from "./CalendarCp";
 import ChatRoomList from "./Chat/ChatRoomList";
