b58dba22e91d4b2e3ee11fac083785eb29fdb2ba
김영섭 <kimyoungseop33@gmail.com>
2024-02-16 05:34:58 +0900

    trip img
    Change-Id: Iba37208936a5dd9809c170949a885cc655c9cb92

--- backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/api/service/TripService.java
@@ -46,4 +46,6 @@ public interface TripService {
     public boolean rejectTripJoinRequest(UserRejectRequest request);
 
     List<TripSimpleResponse> getAllTripIds();
+
+    void putAllTripImgs();
 }
