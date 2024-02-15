e0bba05abdcb269255ec2115541c262f8bdd7db7
se2develop <seahee0130@gmail.com>
2024-02-15 14:38:32 +0900

    Feat: 환불로직 수정
    Change-Id: I6a5cfb659a1e7770d940e1a70e7ece86d8973b79

--- frontend/src/components/Application/SentRequests.jsx
+++ frontend/src/components/Application/SentRequests.jsx
@@ -42,7 +42,6 @@ export default function SentRequests() {
       );
 
       setStartDate(response.data.departureDate);
-      console.log("날짜1" + startDate);
       console.log("날짜2" + response.data.departureDate);
       
       return response.data;
