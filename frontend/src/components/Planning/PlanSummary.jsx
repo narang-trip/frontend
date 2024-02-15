2b035498f8c91b9a0e77c73b944acb3e63e6b4a7
se2develop <seahee0130@gmail.com>
2024-02-15 20:37:47 +0900

    Style: css 수정
    Change-Id: I0a4b7121cb05a6fb2cf2f3efb7271e81159bf717

--- frontend/src/components/Planning/PlanSummary.jsx
+++ frontend/src/components/Planning/PlanSummary.jsx
@@ -19,14 +19,14 @@ const PlanSummary = ({ plan }) => {
       className="relative w-[22%] h-52 rounded-lg bg-yellow-100 p-2 shadow-lg hover:shadow-xl hover:shadow-orange-200 hover:cursor-pointer"
       onClick={goPlan}
     >
-      <div className="flex flex-col w-full border border-yellow-200 h-full rounded-lg bg-white">
+      <div className="flex flex-col w-full h-full bg-white border border-yellow-200 rounded-lg">
         <div className="flex justify-center">
-          <div className="w-1/2 h-6 rounded-b-md border-b-8 border-yellow-200  bg-yellow-100"></div>
+          <div className="w-1/2 h-6 bg-yellow-100 border-b-8 border-yellow-200 rounded-b-md"></div>
         </div>
         <div className="flex-col">
-          <p className="text-2xl text-center pt-1">{plan.planName}</p>
-          <p className="text-sm text-center pt-4">{plan.planDesc}</p>
-          <p className="text-sm text-center pt-1">
+          <p className="pt-1 text-xl font-medium text-center">{plan.planName}</p>
+          <p className="pt-4 text-sm text-center">{plan.planDesc}</p>
+          <p className="px-2 pt-1 text-sm text-center">
             {day - 1}박{day}일
           </p>
           <p className="absolute text-sm bottom-2 right-2">
