17763c85e62f56d7181c40da7b9a3c092d5d6a89
se2develop <seahee0130@gmail.com>
2024-02-15 19:34:43 +0900

    Style: css 수정
    Change-Id: I2d23ee9bc59eb6ab25768bbee47840e6682e2efc

--- frontend/src/components/GoogleMap/PlaceCard.jsx
+++ frontend/src/components/GoogleMap/PlaceCard.jsx
@@ -8,21 +8,36 @@ const PlaceCard = () => {
   return (
     <Droppable droppableId="PlaceCard">
       {(provided) => (
-        <div ref={provided.innerRef} {...provided.droppableProps} className="w-56">
+        <div
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+          className="w-56"
+        >
           {searchResults.map((place, index) => (
-            <Draggable draggableId={`draggable_${index}`} index={index} key={index}>
+            <Draggable
+              draggableId={`draggable_${index}`}
+              index={index}
+              key={index}
+            >
               {(provided) => (
                 <div
                   ref={provided.innerRef}
                   {...provided.draggableProps}
                   {...provided.dragHandleProps}
                 >
-                  <div className="flex justify-center overflow-hidden bg-white rounded-lg">
+                  <div className="flex justify-center overflow-hidden bg-white border rounded-lg border-neutral-200">
                     <div className="flex-col ml-2 text-left">
-                    <img className="h-20 w-60" src={place.photo} />
-                      <div className="text-base font-semibold">{place.name}</div>
+                      <img className="h-20 w-60" src={place.photo} />
+                      <div className="text-base font-semibold">
+                        {place.name}
+                      </div>
+                      <div className="my-1 border border-neutral-200"></div>
                       <div className="text-xs">{place.businessStatus}</div>
+                      <div className="my-1 border border-neutral-200"></div>
+
                       <div className="text-xs">{place.formattedAddress}</div>
+                      <div className="my-1 border border-neutral-200"></div>
+
                       <div className="text-xs">⭐{place.rating}</div>
                     </div>
                   </div>
