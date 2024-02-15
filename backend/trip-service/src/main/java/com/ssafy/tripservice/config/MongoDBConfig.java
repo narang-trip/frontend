623ce4d0771339182e0145ca116205835cb3cf55
1218song <1218song@naver.com>
2024-02-16 03:42:24 +0900

    Refactor: 불필요한 코드 정리 및 로그 처리
    Change-Id: Ic41c8e90f8113c91f5a97d774fae08f6304c21eb

--- backend/trip-service/src/main/java/com/ssafy/tripservice/config/MongoDBConfig.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/config/MongoDBConfig.java
@@ -8,7 +8,6 @@ import org.bson.UuidRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoAdmin;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
--- backend/trip-service/src/main/java/com/ssafy/tripservice/config/S3Config.java
+++ backend/trip-service/src/main/java/com/ssafy/tripservice/config/S3Config.java
@@ -1,6 +1,5 @@
 package com.ssafy.tripservice.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
