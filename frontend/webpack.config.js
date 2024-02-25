b43c1c8ce1a9e5518e08c5c3ac8e43477e7dc479
yhc-key <yhcho0712@gmail.com>
2024-02-25 12:09:53 +0900

    fix : fix error of webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
@@ -6,7 +7,7 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production', // 또는 'development'
-  entry: './src/index.js', // 프로젝트의 진입점 파일
+  entry: './src/main.jsx', // 프로젝트의 진입점 파일을 main.jsx로 변경
   output: {
     filename: 'bundle.js', // 출력될 번들 파일 이름
     path: path.resolve(__dirname, 'dist'), // 번들 파일이 저장될 경로
@@ -31,18 +32,27 @@ module.exports = {
           },
         ],
       },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin(), // JavaScript 최적화
-      new CssMinimizerPlugin(), // CSS 최적화
+      {
+        test: /\.(js|jsx)$/, // JSX 파일을 처리하기 위한 설정 추가
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Babel 로더를 사용하여 JSX를 처리
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // React와 최신 JavaScript 지원
+          },
+        },
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
+    new HtmlWebpackPlugin({
+      template: './index.html', // 웹팩이 index.html을 처리하도록 설정
+    }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'], // .js와 .jsx 파일 모두를 해석할 수 있도록 설정
+  },
 };
