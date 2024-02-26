46ace7310967fa49e39111c746dd235e08c699a7
yhc-key <yhcho0712@gmail.com>
2024-02-26 14:42:26 +0900

    fix : fix carousel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from 'rollup-plugin-visualizer';
-import inspector from 'vite-plugin-inspector';
 
 export default defineConfig({
   server: {
